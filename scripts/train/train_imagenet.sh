52534c5678b0c945f2fb16e36457e0a54583e421
rmaguado <rmorenoaguado3643@gmail.com>
2024-08-01 11:09:52 +0000

    fixed sqrt_wrt_1024 for grad accum

--- scripts/train/train_imagenet.sh
+++ scripts/train/train_imagenet.sh
@@ -3,4 +3,4 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 export PYTHONPATH=.
 torchrun --nproc_per_node=4 ./dinov2/train/train.py \
     --config-file=./configs/imagenet/vits14_reg4.yaml \
-    --output-dir="./runs/gradac_imagenet1k_vits_reg4"
+    --output-dir="./runs/gradac2_imagenet1k_vits_reg4"
