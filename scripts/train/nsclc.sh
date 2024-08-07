c498f25cb4d368f428cc4f1efc302a2d7ef23e87
rmaguado <rmorenoaguado3643@gmail.com>
2024-08-07 22:42:27 +0000

    default parameters

--- scripts/train/nsclc.sh
+++ scripts/train/nsclc.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=1,2
+
+# Default values
+DEFAULT_DEVICES="0,1,2,3"
+DEFAULT_CONFIG_FILE="./configs/nsclc/vits14_reg4.yaml"
+DEFAULT_OUTPUT_DIR="./runs/nsclc"
+
+# Parsing named arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --devices) DEVICES="$2"; shift ;;
+        --config-file) CONFIG_FILE="$2"; shift ;;
+        --output-dir) OUTPUT_DIR="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Use default values if variables are not set
+DEVICES=${DEVICES:-$DEFAULT_DEVICES}
+CONFIG_FILE=${CONFIG_FILE:-$DEFAULT_CONFIG_FILE}
+OUTPUT_DIR=${OUTPUT_DIR:-$DEFAULT_OUTPUT_DIR}
+
+# Export environment variables
+export CUDA_VISIBLE_DEVICES=$DEVICES
 export PYTHONPATH=.
-torchrun --nproc_per_node=2 ./dinov2/train/train.py \
-    --config-file=./configs/nsclc/vits14_reg4.yaml \
-    --output-dir="./runs/nsclc_test"
+
+# Run the torchrun command with the specified or default arguments
+torchrun --nproc_per_node=$(echo $DEVICES | tr -cd ',' | wc -c | awk '{print $1+1}') ./dinov2/train/train.py \
+    --config-file=$CONFIG_FILE \
+    --output-dir=$OUTPUT_DIR
