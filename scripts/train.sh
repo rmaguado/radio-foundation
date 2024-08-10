af913202347b9edb1afcf5d5261b22368dbd0317
rmaguado <rmorenoaguado3643@gmail.com>
2024-08-10 14:55:07 +0000

    new train script format

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,15 +2,15 @@
 
 # Default values
 DEFAULT_DEVICES="0,1,2,3"
-DEFAULT_CONFIG_FILE="./configs/lidc/vits14_reg4.yaml"
-DEFAULT_OUTPUT_DIR="./runs/lidc"
+DEFAULT_OUTPUT_DIR="./runs/default"
+DEFAULT_CONFIG_FILE="./configs/ct_collection/vits14_reg4.yaml"
 
 # Parsing named arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --devices) DEVICES="$2"; shift ;;
-        --config-file) CONFIG_FILE="$2"; shift ;;
-        --output-dir) OUTPUT_DIR="$2"; shift ;;
+        --config) CONFIG_PATH="$2"; shift ;;
+        --output) OUTPUT_DIR="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
@@ -18,8 +18,14 @@ done
 
 # Use default values if variables are not set
 DEVICES=${DEVICES:-$DEFAULT_DEVICES}
-CONFIG_FILE=${CONFIG_FILE:-$DEFAULT_CONFIG_FILE}
-OUTPUT_DIR=${OUTPUT_DIR:-$DEFAULT_OUTPUT_DIR}
+CONFIG_PATH=${CONFIG_PATH:-"ct_collection/vits14_reg4.yaml"}
+OUTPUT_DIR=${OUTPUT_DIR:-"default"}
+
+# Set the full config file path
+CONFIG_FILE="./configs/$CONFIG_PATH"
+
+# Set the full output directory path
+FULL_OUTPUT_DIR="./runs/$OUTPUT_DIR"
 
 # Export environment variables
 export CUDA_VISIBLE_DEVICES=$DEVICES
@@ -28,4 +34,4 @@ export PYTHONPATH=.
 # Run the torchrun command with the specified or default arguments
 torchrun --nproc_per_node=$(echo $DEVICES | tr -cd ',' | wc -c | awk '{print $1+1}') ./dinov2/train/train.py \
     --config-file=$CONFIG_FILE \
-    --output-dir=$OUTPUT_DIR
+    --output-dir=$FULL_OUTPUT_DIR
