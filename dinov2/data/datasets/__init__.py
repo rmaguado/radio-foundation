7f605d176142dbc2b698e802fff36640e09462fb
rmaguado <rmorenoaguado3643@gmail.com>
2024-08-21 08:43:20 +0000

    removed redundant dataset classes

--- dinov2/data/datasets/__init__.py
+++ dinov2/data/datasets/__init__.py
@@ -4,8 +4,6 @@
 # found in the LICENSE file in the root directory of this source tree.
 
 from .image_net import ImageNet
-from .lidc_idri import LidcIdri
-from .nsclc_radiomics import NsclcRadiomics
-from .nsclc_radiogenomics import NsclcRadiogenomics
 from .ct_dataset import CtDataset
 from .ct_collection import CtCollection
+from .ct3d_dataset import Ct3DDataset
