98add00bb4fee35727a3a2a723060c4ad502b8ba
rmaguado <rmorenoaguado3643@gmail.com>
2024-08-22 11:43:47 +0000

    update imports

--- dinov2/data/datasets/__init__.py
+++ dinov2/data/datasets/__init__.py
@@ -5,5 +5,4 @@
 
 from .image_net import ImageNet
 from .ct_dataset import CtDataset
-from .ct_collection import CtCollection
-from .ct3d_dataset import Ct3DDataset
+from .multidataset import MultiDataset
