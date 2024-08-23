64c1adbba53dda982422f44d52164d9bff97fa71
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2024-08-23 15:31:24 +0100

    remove imagenet support

--- datasets/Imagenet-1k/entries.py
+++
@@ -1,9 +0,0 @@
-from dinov2.data.datasets import ImageNet
-
-for split in ImageNet.Split:
-    dataset = ImageNet(
-        split=split,
-        root="./datasets/imagenet/ILSVRC2012/",
-        extra="./datasets/imagenet/extra",
-    )
-    dataset.dump_extra()
