ec9570ff7157a1d39921965e38a5da145f0eb4cd
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2024-10-13 20:15:05 +0100

    apply hounsfield unit conversion

--- dinov2/data/datasets/niftis.py
+++ dinov2/data/datasets/niftis.py
@@ -133,6 +133,14 @@ class NiftiCtDataset(NiftiVolumes):
         volume_data = np.moveaxis(volume_data, axial_dim, 0)
         volume_data = volume_data[slice_index : slice_index + self.channels]
 
+        slope, intercept = nifti_file.get_header().get_slope_inter()
+        if np.isnan(slope):
+            slope = 1.0
+        if np.isnan(intercept):
+            intercept = 0.0
+
+        volume_data = volume_data * slope + intercept
+
         return self.process_ct(volume_data)
 
     def process_ct(self, volume_data: np.ndarray) -> torch.tensor:
