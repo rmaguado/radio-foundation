0a97f65b0fa036d8ea3198b5fd0ab8a57824815f
rmaguado <ruben.morenoaguadog@gmail.com>
2024-10-24 15:23:03 +0200

    setup commands for conda env

---
+++ setup.sh
@@ -0,0 +1,6 @@
+conda create -n dino2t24 python=3.10 -y
+conda activate dino2t24
+conda install pytorch==2.4.1 torchvision torchaudio pytorch-cuda=12.4 -c pytorch -c nvidia -y
+conda install -c xformers xformers -y
+conda install -c conda-forge omegaconf torchmetrics fvcore iopath submitit -y
+pip3 install --extra-index-url https://pypi.nvidia.com cuml-cu12
