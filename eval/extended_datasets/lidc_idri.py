fe61cc129bf1bbbfc0d53ac8b0ed5d06183877cb
rmaguado <ruben.morenoaguadog@gmail.com>
2024-10-24 21:23:21 +0200

    dev idri full volume

--- eval/extended_datasets/lidc_idri.py
+++ eval/extended_datasets/lidc_idri.py
@@ -1,24 +1,79 @@
 import os
 import h5py
 import torch
+import sqlite3
 
-from dinov2.data.datasets import CtDataset
+from dinov2.data.datasets.dicoms import DicomCTVolumesFull
 
 
-class LidcIdriNodules(CtDataset):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs, enable_targets=True)
+def rle_decode(run_lengths, shape):
+    pos = np.cumsum(run_lengths)
+    decoded = np.zeros(np.prod(shape), dtype=np.int32)
+    current_value = 1
+    for i in range(len(pos)):
+        decoded[pos[i]:pos[i+1] if i+1 < len(pos) else None] = current_value
+        current_value = 1 - current_value
+    
+    return decoded.reshape(shape)
 
-    def get_target(self, index: int):
-        entries = self._get_entries()
 
-        series_id = entries[index]["series_id"]
-        slice_index = entries[index]["slice_index"]
+class LidcIdriNodules(DicomCTVolumesFull):
+    def __init__(self, mask_path,*args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        mask_full_path = os.path.join(self.root, series_id, "mask.h5")
+        self.root_mask_path = mask_path
 
-        with h5py.File(mask_full_path, "r") as f:
-            data = f["data"]
-            loaded_mask = data[slice_index]
+    def get_target(self, scanid: str) -> torch.Tensor:
+        
+        mask_path = os.path.join(self.root_mask_path, f"{scanid}.npz")
 
-        return torch.from_numpy(loaded_mask).unsqueeze(0)
+        with load(mask_path) as data:
+            rle_mask = data['rle_mask']
+            shape = data['shape']
+        
+        mask = rle_decode(rle_mask, shape)
+
+        return mask
+
+    def get_image_data(self, index: int) -> Tuple[torch.Tensor, str]:
+        """default copied from dinov2/data/datasets/dicoms.py
+
+        returns image tensor and scanid
+        """
+        dataset_name, rowid = self.entries[index]
+        series_id, scanid = self.cursor.execute(
+            f"SELECT series_id, scanid FROM '{dataset_name}' WHERE rowid = ?",
+            (rowid),
+        ).fetchone()
+
+        self.cursor.execute(
+            """
+            SELECT slice_index, dataset, dicom_path
+            FROM global 
+            WHERE series_id = ? 
+            AND dataset = ?
+            """,
+            (series_id, dataset_name),
+        )
+        slice_indexes_rowid = self.cursor.fetchall()
+        slice_indexes_rowid.sort(key=lambda x: x[0])
+
+        try:
+            stack_data = self.create_stack_data(slice_indexes_rowid)
+        except Exception as e:
+            logger.exception(f"Error processing stack. Seriesid: {series_id} \n{e}")
+            stack_data = torch.zeros((10, 512, 512))
+
+        return stack_data, scanid
+
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, Any]:
+        try:
+            image, scanid = self.get_image_data(index)
+        except Exception as e:
+            raise RuntimeError(f"can not read image for sample {index}") from e
+
+        target = self.get_target(scanid)
+
+        return self.apply_transforms(image, target)
+
+    
