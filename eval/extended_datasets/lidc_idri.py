76e9ef81184ee04559ce0725ccab6faaf3070dce
rmaguado <ruben.morenoaguadog@gmail.com>
2024-11-08 19:59:52 +0100

    update imports

--- eval/extended_datasets/lidc_idri.py
+++ eval/extended_datasets/lidc_idri.py
@@ -1,7 +1,6 @@
 import os
-import h5py
 import torch
-import sqlite3
+from typing import Tuple, Any
 
 from dinov2.data.datasets.dicoms import DicomCTVolumesFull
 
@@ -11,26 +10,26 @@ def rle_decode(run_lengths, shape):
     decoded = np.zeros(np.prod(shape), dtype=np.int32)
     current_value = 1
     for i in range(len(pos)):
-        decoded[pos[i]:pos[i+1] if i+1 < len(pos) else None] = current_value
+        decoded[pos[i] : pos[i + 1] if i + 1 < len(pos) else None] = current_value
         current_value = 1 - current_value
-    
+
     return decoded.reshape(shape)
 
 
 class LidcIdriNodules(DicomCTVolumesFull):
-    def __init__(self, mask_path,*args, **kwargs):
+    def __init__(self, mask_path, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.root_mask_path = mask_path
 
     def get_target(self, scanid: str) -> torch.Tensor:
-        
+
         mask_path = os.path.join(self.root_mask_path, f"{scanid}.npz")
 
         with load(mask_path) as data:
-            rle_mask = data['rle_mask']
-            shape = data['shape']
-        
+            rle_mask = data["rle_mask"]
+            shape = data["shape"]
+
         mask = rle_decode(rle_mask, shape)
 
         return mask
@@ -75,5 +74,3 @@ class LidcIdriNodules(DicomCTVolumesFull):
         target = self.get_target(scanid)
 
         return self.apply_transforms(image, target)
-
-    
