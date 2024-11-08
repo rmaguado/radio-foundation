0e10098801350dd3b13d43e44127eb9499339921
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2024-11-08 18:12:53 +0000

    update statistics script for nifti (needs testing)

--- data/statistics.py
+++ data/statistics.py
@@ -5,6 +5,7 @@ Samples the mean and standard deviation of an image dataset.
 import os
 import argparse
 import pydicom
+import nibabel as nib
 import numpy as np
 import sqlite3
 from tqdm import tqdm
@@ -33,8 +34,37 @@ def read_dicom_image(
         array_data = np.clip(array_data, lower, upper)
         return array_data
     except Exception as e:
-        logger.error(f"Error reading {dcm_path}: {e}")
-        return None
+        logger.exception(f"Error reading {abs_path}: {e}")
+    return None
+
+
+def read_nifti_image(
+    root_path: str,
+    dataset_name: str,
+    nii_path: str,
+    lower: float = -1000,
+    upper: float = 1900,
+) -> np.ndarray:
+    try:
+        abs_path = os.path.join(root_path, dataset_name, nii_path)
+        nii = nib.load(abs_path)
+        array_data = nii.get_fdata()
+
+        rescale_slope = nii.dataobj.slope
+        rescale_intercept = nii.dataobj.inter
+
+        if np.isnan(rescale_slope):
+            rescale_slope = 1.0
+        if np.isnan(rescale_intercept):
+            rescale_intercept = 0.0
+
+        array_data = array_data * rescale_slope + rescale_intercept
+
+        array_data = np.clip(array_data, lower, upper)
+        return array_data
+    except Exception as e:
+        logger.exception(f"Error reading {abs_path}: {e}")
+    return None
 
 
 def get_mean_std(pixel_array: np.ndarray) -> tuple:
@@ -43,28 +73,28 @@ def get_mean_std(pixel_array: np.ndarray) -> tuple:
     return mean, std
 
 
-def sample_paths_dicom(conn, cursor, n: int) -> Tuple[float, flaot]:
-    raise RuntimeError(
-        "Outdated function. Needs to be rewritten for new database format."
-    )
+def sample_paths_dicom(cursor, root_path: str, n: int) -> Tuple[float, float]:
+    cursor.execute("SELECT dataset, dcm_path FROM datasets;")
+    datasets = cursor.fetchall()
 
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    datasets = [table[0] for table in cursor.fetchall() if table[0] != "global"]
-    logger.info(f"Sampling from datasets: {', '.join(datasets)}")
+    if len(datasets) == 0:
+        raise ValueError("No datasets found in the database.")
 
-    n_dataset = n // len(datasets)
+    samples_per_dataset = n // len(datasets)
 
-    paths = []
-    for dataset in datasets:
+    dcm_paths = []
+    for dataset_name in datasets:
         cursor.execute(
-            f"SELECT dicom_path FROM global WHERE dataset = '{dataset}' ORDER BY RANDOM() LIMIT {n_dataset}"
+            f"""
+            SELECT dicom_path FROM global WHERE dataset = '{dataset_name}'
+            ORDER BY RANDOM()
+            LIMIT {samples_per_dataset};
+            """
         )
-        dcm_paths = cursor.fetchall()
-        paths += [(dataset, dcm_path[0]) for dcm_path in dcm_paths]
+        dcm_paths.extend([(dataset_name, dcm_path) for dcm_path in cursor.fetchall()])
 
     means = []
     variances = []
-
     for dataset, dcm_path in tqdm(dcm_paths):
         pixel_array = read_dicom_image(root_path, dataset, dcm_path)
         if pixel_array is not None:
@@ -78,12 +108,49 @@ def sample_paths_dicom(conn, cursor, n: int) -> Tuple[float, flaot]:
     return mean, std
 
 
-def sample_paths_nifti(conn, cursor, n: int) -> Tuple[float, float]:
-    raise NotImplementedError
+def sample_paths_nifti(cursor, root_path: str, n: int) -> Tuple[float, float]:
+    cursor.execute("SELECT dataset, dcm_path FROM datasets;")
+    datasets = cursor.fetchall()
+
+    if len(datasets) == 0:
+        raise ValueError("No datasets found in the database.")
+
+    samples_per_dataset = n // len(datasets)
+
+    nii_paths = []
+    for dataset_name in datasets:
+        cursor.execute(
+            f"""
+            SELECT nifti_path FROM global WHERE dataset = '{dataset_name}'
+            ORDER BY RANDOM()
+            LIMIT {samples_per_dataset};
+            """
+        )
+        nii_paths.extend([(dataset_name, nii_path) for nii_path in cursor.fetchall()])
+
+    means = []
+    variances = []
+    for dataset, nii_path in tqdm(nii_paths):
+        pixel_array = read_nifti_image(root_path, dataset, nii_path)
+        if pixel_array is not None:
+            mean, std = get_mean_std(pixel_array)
+            means.append(mean)
+            variances.append(std**2)
+
+    mean = np.mean(means)
+    std = np.mean(variances) ** 0.5
+
+    return mean, std
 
 
-def get_database_storate(conn, cursor):
-    raise NotImplementedError
+def get_database_storate(cursor):
+    cursor.execute("SELECT value FROM metadata WHERE key = 'storage';")
+    storage_type = cursor.fetchone()[0]
+
+    if storage_type not in ["dicom", "nifti"]:
+        raise ValueError(f"Unknown storage type: {storage_type}")
+
+    return storage_type
 
 
 def get_argpase():
@@ -112,6 +179,7 @@ def get_argpase():
 
 def main(args):
     set_logging("data/log/statistics.log")
+
     root_path = args.root_path
     n = args.n
 
@@ -122,12 +190,12 @@ def main(args):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    storage_type = get_database_storate(conn, cursor)
+    storage_type = get_database_storate(cursor)
 
     if storage_type == "dicom":
-        mean, std = sample_paths_dicom(conn, cursor, n)
+        mean, std = sample_paths_dicom(cursor, root_path, n)
     elif storage_type == "nifti":
-        mean, std = sample_paths_nifti(conn, cursor, n)
+        mean, std = sample_paths_nifti(cursor, root_path, n)
 
     conn.close()
 
