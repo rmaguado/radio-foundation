2f5b5486a6cf6a91a2bae0587cc8d3039c8bd4e1
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-01-06 16:41:40 +0000

    add AggregateClassTokens

--- evaluation/utils/classification.py
+++ evaluation/utils/classification.py
@@ -19,19 +19,34 @@ class ImageTransform:
 class AggregateClassTokens(nn.Module):
     def __init__(
         self,
-        feature_model,
-        embed_dim=384 * 4,
-        hidden_dim=2048,
+        feature_dim=384 * 4,
+        embed_dim=2048,
         num_labels=2,
         device=torch.device("cpu"),
     ):
         super().__init__()
+        self.multihead_attn = nn.MultiheadAttention(
+            embed_dim=embed_dim, num_heads=8, kdim=feature_dim, vdim=feature_dim
+        )
+        self.cls_token = nn.Parameter(torch.randn(1, 1, feature_dim))
+
+        self.classifier = nn.Linear(feature_dim, num_labels).to(device)
+
+    def forward(self, features):
+        cls_tokens = self.cls_token.expand(features.size(1), -1, -1)
+        cls_tokens = cls_tokens.permute(1, 0, 2)
+
+        attn_output, _ = self.multihead_attn(cls_tokens, features, features)
+        attn_output = attn_output.permute(1, 0, 2)
+
+        return self.classifier(attn_output)
 
 
 def _genertic_dataloader(dataset, is_infinite=False):
     """
-    Generic Dataloader for classification with a single slice 
+    Generic Dataloader for classification with a single slice
     """
+
     def collate_fn(inputs):
         images = torch.stack([x[0] for x in inputs], dim=0)
         labels = torch.stack([x[1] for x in inputs], dim=0)
