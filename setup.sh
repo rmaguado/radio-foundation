023a2eb2a5cc477972e61a93263d2aff571743e9
rmaguado <ruben.morenoaguado@gmail.com>
2025-02-23 17:48:11 +0000

    rename conda env

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-conda create -n dino2t24 python=3.10 -y
-conda activate dino2t24
+conda create -n radio python=3.10 -y
+conda activate radio
 conda install pytorch==2.4.1 torchvision torchaudio pytorch-cuda=12.4 -c pytorch -c nvidia -y
 conda install -c xformers xformers -y
 conda install -c conda-forge omegaconf torchmetrics fvcore iopath submitit -y
