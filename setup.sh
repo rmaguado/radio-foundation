3a4015e9c52fab27d871d8e4c24c062a89baae1e
rmaguado <ruben.morenoaguadog@gmail.com>
2025-03-06 15:55:14 +0100

    update environment setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
-conda create -n radio python=3.10 -y
-conda activate radio
+conda create -n dino2 python=3.10 -y
+conda activate dino2
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
-pip3 install -U xformers --index-url https://download.pytorch.org/whl/cu118
+pip3 install -U xformers --index-url https://download.pytorch.org/whl/cu126
 conda install -c conda-forge omegaconf torchmetrics fvcore iopath submitit -y
-pip3 install --extra-index-url https://pypi.nvidia.com cuml-cu12
 pip3 install -r requirements.txt
