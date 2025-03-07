00096732441a84719b6c08658244b34630dd1d6e
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-03-07 13:57:17 +0000

    test with optim FSDP api

--- dinov2/fsdp/__init__.py
+++ dinov2/fsdp/__init__.py
@@ -2,45 +2,14 @@
 #
 # This source code is licensed under the Apache License, Version 2.0
 # found in the LICENSE file in the root directory of this source tree.
-#
-# AntiFSDPCheckpointer implemented by TimDarcet:
-# https://gist.github.com/TimDarcet/8b986c6b2b2522603fe57a4327590652
 
 import os
-from typing import Any, Dict, List, Optional
-import glob
-import io
-import pickle
-import warnings
+from typing import Any
 
 import torch
-import torch.distributed._shard
-from fvcore.common.checkpoint import (
-    Checkpointer,
-    TORCH_VERSION,
-    FakeQuantizeBase,
-    ObserverBase,
-    _IncompatibleKeys,
-    _strip_prefix_if_present,
-    nn,
-    quantization,
-)
-from torch.serialization import (
-    FILE_LIKE,
-    MAP_LOCATION,
-    StorageType,
-    _check_dill_version,
-    _get_restore_location,
-    _is_torchscript_zip,
-    _is_zipfile,
-    _maybe_decode_ascii,
-    _open_file_like,
-    _open_zipfile_reader,
-    _weights_only_unpickler,
-)
-from tqdm.auto import tqdm
-
+import dinov2.distributed as distributed
 from functools import partial
+from fvcore.common.checkpoint import Checkpointer
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.distributed.fsdp import ShardingStrategy
 from torch.distributed.fsdp import MixedPrecision
@@ -48,8 +17,6 @@ from torch.distributed.fsdp import StateDictType
 from torch.distributed.fsdp.sharded_grad_scaler import ShardedGradScaler
 from torch.distributed.fsdp.wrap import ModuleWrapPolicy
 from torch.distributed.fsdp._runtime_utils import _reshard
-
-import dinov2.distributed as distributed
 
 
 def get_fsdp_wrapper(model_cfg, modules_to_wrap=set()):
@@ -97,9 +64,8 @@ def is_sharded_fsdp(x):
 
 def free_if_fsdp(x):
     if is_sharded_fsdp(x):
-        handles = x._handles
-        true_list = [True for h in handles]
-        _reshard(x, handles, true_list)
+        handle = x._handle
+        _reshard(x, handle, True)
 
 
 def get_fsdp_modules(x):
@@ -119,6 +85,7 @@ class FSDPCheckpointer(Checkpointer):
     def save(self, name: str, **kwargs: Any) -> None:
         """
         Dump model and checkpointables to a file.
+
         Args:
             name (str): name of the file.
             kwargs (dict): extra arbitrary data to save.
@@ -174,6 +141,7 @@ class FSDPCheckpointer(Checkpointer):
     def tag_last_checkpoint(self, last_filename_basename: str) -> None:
         """
         Tag the last checkpoint.
+
         Args:
             last_filename_basename (str): the basename of the last filename.
         """
@@ -184,298 +152,79 @@ class FSDPCheckpointer(Checkpointer):
             f.write(last_filename_basename)  # pyre-ignore
 
 
-ShardedGradScaler = ShardedGradScaler
-
-
-class ShardedTensor(torch.distributed._shard.sharded_tensor.api.ShardedTensor):
-    def __setstate__(self, state):
-        (
-            self._local_shards,
-            self._metadata,
-            pg_state,
-            self._sharding_spec,
-            self._init_rrefs,
-        ) = state
-
-
-def _load_monke(
-    zip_file, map_location, pickle_module, pickle_file="data.pkl", **pickle_load_args
-):
-    restore_location = _get_restore_location(map_location)
-    loaded_storages = {}
-
-    def load_tensor(dtype, numel, key, location):
-        name = f"data/{key}"
-        storage = (
-            zip_file.get_storage_from_record(name, numel, torch.UntypedStorage)
-            ._typed_storage()
-            ._untyped_storage
-        )
-        # TODO: Once we decide to break serialization FC, we can
-        # stop wrapping with TypedStorage
-        typed_storage = torch.storage.TypedStorage(
-            wrap_storage=restore_location(storage, location),
-            dtype=dtype,
-            _internal=True,
-        )
-        if typed_storage._data_ptr() != 0:
-            loaded_storages[key] = typed_storage
-        return typed_storage
-
-    def persistent_load(saved_id):
-        assert isinstance(saved_id, tuple)
-        typename = _maybe_decode_ascii(saved_id[0])
-        data = saved_id[1:]
-        assert (
-            typename == "storage"
-        ), f"Unknown typename for persistent_load, expected 'storage' but got '{typename}'"
-        storage_type, key, location, numel = data
-        if storage_type is torch.UntypedStorage:
-            dtype = torch.uint8
-        else:
-            dtype = storage_type.dtype
-        if key in loaded_storages:
-            typed_storage = loaded_storages[key]
-        else:
-            nbytes = numel * torch._utils._element_size(dtype)
-            typed_storage = load_tensor(
-                dtype, nbytes, key, _maybe_decode_ascii(location)
-            )
-        return typed_storage
-
-    load_module_mapping: Dict[str, str] = {
-        # See https://github.com/pytorch/pytorch/pull/51633
-        "torch.tensor": "torch._tensor",
-        "torch.distributed._shard.sharded_tensor.api": __name__,
-    }
-
-    # Need to subclass Unpickler instead of directly monkey-patching the find_class method
-    # because it's marked readonly in pickle.
-    # The type: ignore is because mypy can't statically determine the type of this class.
-    class UnpicklerWrapper(pickle_module.Unpickler):  # type: ignore[name-defined]
-        # from https://stackoverflow.com/questions/13398462/unpickling-python-objects-with-a-changed-module-path/13405732
-        # Lets us override the imports that pickle uses when unpickling an object.
-        # This is useful for maintaining BC if we change a module path that tensor instantiation relies on.
-        def find_class(self, mod_name, name):
-            if type(name) is str and "Storage" in name:
-                try:
-                    return StorageType(name)
-                except KeyError:
-                    pass
-            # print(mod_name, name)
-            mod_name = load_module_mapping.get(mod_name, mod_name)
-            return super().find_class(mod_name, name)
-
-    # Load the data (which may in turn use `persistent_load` to load tensors)
-    data_file = io.BytesIO(zip_file.get_record(pickle_file))
-    unpickler = UnpicklerWrapper(data_file, **pickle_load_args)
-    unpickler.persistent_load = persistent_load
-    result = unpickler.load()
-    torch._utils._validate_loaded_sparse_tensors()
-    return result
-
-
-def torch_load_monke(
-    f: FILE_LIKE,
-    map_location: MAP_LOCATION = None,
-    pickle_module: Any = None,
-    *,
-    weights_only: bool = False,
-    **pickle_load_args: Any,
-) -> Any:
-    torch._C._log_api_usage_once("torch.load")
-    UNSAFE_MESSAGE = (
-        "Weights only load failed. Re-running `torch.load` with `weights_only` set to `False`"
-        " will likely succeed, but it can result in arbitrary code execution."
-        "Do it only if you get the file from a trusted source. WeightsUnpickler error: "
-    )
-    # Add ability to force safe only weight loads via environment variable
-    if os.getenv("TORCH_FORCE_WEIGHTS_ONLY_LOAD", "0").lower() in [
-        "1",
-        "y",
-        "yes",
-        "true",
-    ]:
-        weights_only = True
-    if weights_only:
-        if pickle_module is not None:
-            raise RuntimeError(
-                "Can not safely load weights when explicit pickle_module is specified"
-            )
-    else:
-        if pickle_module is None:
-            pickle_module = pickle
-    _check_dill_version(pickle_module)
-    if "encoding" not in pickle_load_args.keys():
-        pickle_load_args["encoding"] = "utf-8"
-    with _open_file_like(f, "rb") as opened_file:
-        if _is_zipfile(opened_file):
-            # The zipfile reader is going to advance the current file position.
-            # If we want to actually tail call to torch.jit.load, we need to
-            # reset back to the original position.
-            orig_position = opened_file.tell()
-            with _open_zipfile_reader(opened_file) as opened_zipfile:
-                if _is_torchscript_zip(opened_zipfile):
-                    warnings.warn(
-                        "'torch.load' received a zip file that looks like a TorchScript archive"
-                        " dispatching to 'torch.jit.load' (call 'torch.jit.load' directly to"
-                        " silence this warning)",
-                        UserWarning,
-                    )
-                    opened_file.seek(orig_position)
-                    return torch.jit.load(opened_file, map_location=map_location)
-                if weights_only:
-                    try:
-                        return _load_monke(
-                            opened_zipfile,
-                            map_location,
-                            _weights_only_unpickler,
-                            **pickle_load_args,
-                        )
-                    except RuntimeError as e:
-                        raise pickle.UnpicklingError(UNSAFE_MESSAGE + str(e)) from None
-                return _load_monke(
-                    opened_zipfile, map_location, pickle_module, **pickle_load_args
-                )
-
-
-def recursive_fuse(shards):
-    if isinstance(shards[0], ShardedTensor):
-        return shards[0]
-    elif isinstance(shards[0], torch.Tensor):
-        assert all(isinstance(s, torch.Tensor) for s in shards)
-        return torch.cat(shards)
-    elif isinstance(shards[0], dict):
-        assert all(isinstance(s, dict) for s in shards)
-        all_keys = set.union(*map(lambda s: set(s.keys()), shards))
-        return {k: recursive_fuse([s[k] for s in shards if k in s]) for k in all_keys}
-    else:
-        assert all(s == shards[0] for s in shards)
-        return shards[0]
-
-
-class AntiFSDPCheckpointer(FSDPCheckpointer):
-    def load(
-        self, path: str, checkpointables: Optional[List[str]] = None
-    ) -> Dict[str, Any]:
+class FlexibleFSDPCheckpointer(Checkpointer):
+    def save(self, name: str, **kwargs: Any) -> None:
         """
-        Load from the given checkpoint.
+        Dump model and checkpointables to a file.
+
         Args:
-            path (str): path or url to the checkpoint. If empty, will not load
-                anything.
-            checkpointables (list): List of checkpointable names to load. If not
-                specified (None), will load all the possible checkpointables.
-        Returns:
-            dict:
-                extra data loaded from the checkpoint that has not been
-                processed. For example, those saved with
-                :meth:`.save(**extra_data)`.
+            name (str): name of the file.
+            kwargs (dict): extra arbitrary data to save.
         """
+        if not self.save_dir or not self.save_to_disk:
+            return
+
+        data = {}
+        with FSDP.state_dict_type(self.model, StateDictType.FULL_STATE_DICT):
+            data["model"] = self.model.state_dict()
+
+        for key, obj in self.checkpointables.items():
+            if key == "optimizer":
+                with FSDP.state_dict_type(self.model, StateDictType.FULL_STATE_DICT):
+                    optim_state_dict = FSDP.optim_state_dict(
+                        self.model, obj.state_dict()
+                    )
+                    data[key] = optim_state_dict
+            else:
+                data[key] = obj.state_dict()
+        data.update(kwargs)
+
+        if distributed.get_global_rank() != 0:
+            torch.distributed.barrier()
+            return
+
+        basename = f"{name}.pth"
+        save_file = os.path.join(self.save_dir, basename)
+        assert os.path.basename(save_file) == basename, basename
+        self.logger.info("Saving checkpoint to {}".format(save_file))
+        with self.path_manager.open(save_file, "wb") as f:
+            torch.save(data, f)
+        self.tag_last_checkpoint(basename)
+
+        torch.distributed.barrier()
+
+    def load(self, path, checkpointables=None):
         if not path:
-            # no checkpoint provided
             self.logger.info("No checkpoint found. Initializing model from scratch")
             return {}
-        all_paths = glob.glob(path)
-        self.logger.info("[Checkpointer] Loading from {} ...".format(all_paths))
-        # path may not be a local file, but _load_file is responsible to handle it.
-        shards = []
-        for path in tqdm(all_paths):
-            shards.append(torch_load_monke(path, map_location="cpu"))
-        checkpoint = recursive_fuse(shards)
-        incompatible = self._load_model(checkpoint)
-        if (
-            incompatible is not None
-        ):  # handle some existing subclasses that returns None
+        self.logger.info("[Checkpointer] Loading from {} ...".format(path))
+        if not os.path.isfile(path):
+            path = self.path_manager.get_local_path(path)
+            assert os.path.isfile(path), "Checkpoint {} not found!".format(path)
+
+        checkpoint = self._load_file(path)
+        with FSDP.state_dict_type(self.model, StateDictType.FULL_STATE_DICT):
+            incompatible = self._load_model(checkpoint)
+        if incompatible is not None:
             self._log_incompatible_keys(incompatible)
 
         for key in self.checkpointables if checkpointables is None else checkpointables:
             if key in checkpoint:
                 self.logger.info("Loading {} from {} ...".format(key, path))
                 obj = self.checkpointables[key]
-                obj.load_state_dict(checkpoint.pop(key))
+                if key == "optimizer":
+                    with FSDP.state_dict_type(
+                        self.model, StateDictType.FULL_STATE_DICT
+                    ):
+                        optim_state_dict = checkpoint.pop(key)
+                        optim_state_dict = FSDP.optim_state_dict_to_load(
+                            self.model, obj, optim_state_dict
+                        )
+                        obj.load_state_dict(optim_state_dict)
+                else:
+                    obj.load_state_dict(checkpoint.pop(key))
 
-        # return any further checkpoint data
         return checkpoint
 
-    def _load_model(self, checkpoint: Any) -> _IncompatibleKeys:
-        """
-        Load weights from a checkpoint.
-        Reshape if needed
-        Args:
-            checkpoint (Any): checkpoint contains the weights.
-        Returns:
-            ``NamedTuple`` with ``missing_keys``, ``unexpected_keys``,
-                and ``incorrect_shapes`` fields:
-                * **missing_keys** is a list of str containing the missing keys
-                * **unexpected_keys** is a list of str containing the unexpected keys
-                * **incorrect_shapes** is a list of (key, shape in checkpoint, shape in model)
-        """
-        checkpoint_state_dict = checkpoint.pop("model")
-        self._convert_ndarray_to_tensor(checkpoint_state_dict)
 
-        # if the state_dict comes from a model that was wrapped in a
-        # DataParallel or DistributedDataParallel during serialization,
-        # remove the "module" prefix before performing the matching.
-        _strip_prefix_if_present(checkpoint_state_dict, "module.")
-
-        # workaround https://github.com/pytorch/pytorch/issues/24139
-        model_state_dict = self.model.state_dict()
-        incorrect_shapes = []
-        for k in list(checkpoint_state_dict.keys()):
-            if k in model_state_dict:
-                model_param = model_state_dict[k]
-                # Allow mismatch for uninitialized parameters
-                if TORCH_VERSION >= (1, 8) and isinstance(
-                    model_param, nn.parameter.UninitializedParameter
-                ):
-                    continue
-                shape_model = tuple(model_param.shape)
-                shape_checkpoint = tuple(checkpoint_state_dict[k].shape)
-                if shape_model != shape_checkpoint:
-                    if model_param.numel() == checkpoint_state_dict[k].numel():
-                        # print(f"reshaping: {k}, {shape_model}, {shape_checkpoint}")
-                        checkpoint_state_dict[k] = checkpoint_state_dict[k].reshape(
-                            shape_model
-                        )
-                        shape_checkpoint = tuple(checkpoint_state_dict[k].shape)
-                    else:
-                        # print(f"incorrect shape: {k}, {shape_model}, {shape_checkpoint}")
-                        pass
-                if shape_model != shape_checkpoint:
-                    has_observer_base_classes = (
-                        TORCH_VERSION >= (1, 8)
-                        and hasattr(quantization, "ObserverBase")
-                        and hasattr(quantization, "FakeQuantizeBase")
-                    )
-                    if has_observer_base_classes:
-                        # Handle the special case of quantization per channel observers,
-                        # where buffer shape mismatches are expected.
-                        def _get_module_for_key(
-                            model: torch.nn.Module, key: str
-                        ) -> torch.nn.Module:
-                            # foo.bar.param_or_buffer_name -> [foo, bar]
-                            key_parts = key.split(".")[:-1]
-                            cur_module = model
-                            for key_part in key_parts:
-                                cur_module = getattr(cur_module, key_part)
-                            return cur_module
-
-                        cls_to_skip = (
-                            ObserverBase,
-                            FakeQuantizeBase,
-                        )
-                        target_module = _get_module_for_key(self.model, k)
-                        if isinstance(target_module, cls_to_skip):
-                            # Do not remove modules with expected shape mismatches
-                            # them from the state_dict loading. They have special logic
-                            # in _load_from_state_dict to handle the mismatches.
-                            continue
-
-                    incorrect_shapes.append((k, shape_checkpoint, shape_model))
-                    checkpoint_state_dict.pop(k)
-        incompatible = self.model.load_state_dict(checkpoint_state_dict, strict=False)
-        return _IncompatibleKeys(
-            missing_keys=incompatible.missing_keys,
-            unexpected_keys=incompatible.unexpected_keys,
-            incorrect_shapes=incorrect_shapes,
-        )
+ShardedGradScaler = ShardedGradScaler
--- dinov2/train/setup.py
+++ dinov2/train/setup.py
@@ -4,7 +4,7 @@ from functools import partial
 
 from fvcore.common.checkpoint import PeriodicCheckpointer
 
-from dinov2.fsdp import FSDPCheckpointer, AntiFSDPCheckpointer
+from dinov2.fsdp import FSDPCheckpointer, FlexibleFSDPCheckpointer
 from dinov2.utils.utils import CosineScheduler
 from dinov2.data import collate_data_and_cast, MaskingGenerator
 from dinov2.data import SamplerType, make_data_loader, make_train_dataset
@@ -134,7 +134,7 @@ def setup_training_components(cfg, model, resume):
     logger.info("Schedulers ready.")
 
     # sharding_strategy = cfg.compute_precision.teacher.backbone.sharding_strategy
-    checkpointer_wrapper = FSDPCheckpointer
+    checkpointer_wrapper = FlexibleFSDPCheckpointer
 
     checkpointer = checkpointer_wrapper(
         model, cfg.train.output_dir, optimizer=optimizer, save_to_disk=True
