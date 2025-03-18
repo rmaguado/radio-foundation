0f2bb62b0a3586926f7881fc0b81a7257c29f26c
rmaguado <ruben.morenoaguadog@gmail.com>
2025-03-18 12:42:07 +0100

    remove f1 approx from metrics

--- evaluation/utils/metrics.py
+++ evaluation/utils/metrics.py
@@ -1,31 +1,20 @@
-from sklearn.metrics import (
-    roc_auc_score,
-    average_precision_score,
-    f1_score,
-)
+from sklearn.metrics import roc_auc_score, average_precision_score
 
 import os
 import numpy as np
 
 
 def compute_metrics(logits, labels):
-    threshold = 0.5
     probabilities = 1 / (1 + np.exp(-logits))
-    binary_predictions = probabilities >= threshold
 
     roc_auc = roc_auc_score(labels, probabilities)
     pr_auc = average_precision_score(labels, probabilities)
-    f1 = f1_score(labels, binary_predictions)
 
-    return {
-        "roc_auc": roc_auc,
-        "pr_auc": pr_auc,
-        "f1": f1,
-    }
+    return {"roc_auc": roc_auc, "pr_auc": pr_auc}
 
 
 def save_metrics(metrics_dict, output_path, filename):
-    metrics = ["roc_auc", "pr_auc", "f1"]
+    metrics = ["roc_auc", "pr_auc"]
 
     metrics_path = os.path.join(output_path, f"{filename}.csv")
     os.makedirs(os.path.dirname(metrics_path), exist_ok=True)
@@ -37,7 +26,7 @@ def save_metrics(metrics_dict, output_path, filename):
 
 
 def print_metrics(metrics_dict, field: str):
-    metrics = ["roc_auc", "pr_auc", "f1"]
+    metrics = ["roc_auc", "pr_auc"]
     metrics_str = " - ".join(
         [f"{metric}: {metrics_dict[metric]:.4f}" for metric in metrics]
     )
