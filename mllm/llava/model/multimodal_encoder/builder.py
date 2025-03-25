45aadf2e3b68d798f6bfd71d46cab2c8cf02ec5f
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-03-25 17:34:27 +0100

    add dino vision tower

--- mllm/llava/model/multimodal_encoder/builder.py
+++ mllm/llava/model/multimodal_encoder/builder.py
@@ -1,15 +1,24 @@
 import os
 from .clip_encoder import CLIPVisionTower, CLIPVisionTowerS2
+from .dino_encoder import DINOVisionTower
 
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
-    vision_tower = getattr(vision_tower_cfg, 'mm_vision_tower', getattr(vision_tower_cfg, 'vision_tower', None))
-    is_absolute_path_exists = os.path.exists(vision_tower)
-    use_s2 = getattr(vision_tower_cfg, 's2', False)
-    if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower:
-        if use_s2:
-            return CLIPVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
-        else:
-            return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    vision_tower_name = getattr(
+        vision_tower_cfg,
+        "mm_vision_tower",
+        getattr(vision_tower_cfg, "vision_tower", None),
+    )
 
-    raise ValueError(f'Unknown vision tower: {vision_tower}')
+    vision_tower_type = getattr(vision_tower_cfg, "vision_tower_type", None)
+
+    if vision_tower_type == "dino":
+        return DINOVisionTower(vision_tower_name, args=vision_tower_cfg, **kwargs)
+    elif vision_tower_type == "clip":
+        use_s2 = getattr(vision_tower_cfg, "s2", False)
+        if use_s2:
+            return CLIPVisionTowerS2(vision_tower_name, args=vision_tower_cfg, **kwargs)
+        else:
+            return CLIPVisionTower(vision_tower_name, args=vision_tower_cfg, **kwargs)
+
+    raise ValueError(f"Unknown vision tower: {vision_tower_type}")
---
+++ mllm/llava/model/multimodal_encoder/dino_encoder.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import OmegaConf
+from functools import partial
+
+from dinov2.models import build_model_from_cfg
+from dinov2.utils.utils import load_pretrained_weights
+
+
+class DINOVisionTower(nn.Module):
+    def __init__(self, vision_tower, args, delay_load=False):
+        super().__init__()
+
+        self.is_loaded = False
+
+        self.config = OmegaConf.load(args.mm_vision_config_path)
+        self.vision_tower_name = vision_tower
+        self.select_layer = args.mm_vision_select_layer
+        self.select_feature = getattr(args, "mm_vision_select_feature", "patch")
+
+        self.use_cls = self.select_feature == "cls_patch"
+        self.path_to_checkpoint = args.mm_vision_checkpoint_path
+
+        self.load_model()
+
+        if not delay_load:
+            self.load_model()
+        elif getattr(args, "unfreeze_mm_vision_tower", False):
+            self.load_model()
+        else:
+            self.cfg_only = self.config
+
+    def load_model(self):
+        if self.is_loaded:
+            print(
+                "{} is already loaded, `load_model` called again, skipping.".format(
+                    self.vision_tower_name
+                )
+            )
+            return
+
+        self.vision_tower = build_model_from_cfg(self.config, only_teacher=True)
+        load_pretrained_weights(self.vision_tower, self.path_to_checkpoint, "teacher")
+
+        teacher_dtype_str = (
+            self.config.compute_precision.teacher.backbone.mixed_precision.param_dtype
+        )
+        if teacher_dtype_str == "fp16":
+            autocast_dtype = torch.half
+        elif teacher_dtype_str == "bf16":
+            autocast_dtype = torch.bfloat16
+        else:
+            autocast_dtype = torch.float
+
+        self.autocast_ctx = partial(
+            torch.autocast, enabled=True, dtype=autocast_dtype, device_type="cuda"
+        )
+
+        self.vision_tower.requires_grad_(False)
+
+        self.is_loaded = True
+
+    @torch.no_grad()
+    def forward(self, images):
+        with self.autocast_ctx():
+            features = self.feature_model.get_intermediate_layers(
+                images, [self.select_layer], return_class_token=self.use_cls
+            )
+
+        return features
+
+    @property
+    def dummy_feature(self):
+        return torch.zeros(1, self.hidden_size, device=self.device, dtype=self.dtype)
+
+    @property
+    def dtype(self):
+        return self.vision_tower.dtype
+
+    @property
+    def device(self):
+        return self.vision_tower.device
+
+    @property
+    def config(self):
+        if self.is_loaded:
+            return self.vision_tower.config
+        else:
+            return self.cfg_only
+
+    @property
+    def hidden_size(self):
+        return self.config.student.full_image_size
+
+    @property
+    def num_patches_per_side(self):
+        return self.config.student.full_image_size // self.config.student.patch_size
+
+    @property
+    def num_patches(self):
+        return (
+            self.config.student.full_image_size // self.config.student.patch_size
+        ) ** 2
