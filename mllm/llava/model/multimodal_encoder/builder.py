7740c7c1c07c1729ed96304845cdefe663fb1885
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-03-26 11:21:34 +0100

    remove clip support

--- mllm/llava/model/multimodal_encoder/builder.py
+++ mllm/llava/model/multimodal_encoder/builder.py
@@ -1,23 +1,11 @@
-from .clip_encoder import CLIPVisionTower, CLIPVisionTowerS2
 from .dino_encoder import DINOVisionTower
 
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
-    vision_tower_name = getattr(
+    vision_tower = getattr(
         vision_tower_cfg,
         "mm_vision_tower",
         getattr(vision_tower_cfg, "vision_tower", None),
     )
 
-    vision_tower_type = getattr(vision_tower_cfg, "vision_tower_type", None)
-
-    if vision_tower_type == "dino":
-        return DINOVisionTower(vision_tower_name, args=vision_tower_cfg, **kwargs)
-    elif vision_tower_type == "clip":
-        use_s2 = getattr(vision_tower_cfg, "s2", False)
-        if use_s2:
-            return CLIPVisionTowerS2(vision_tower_name, args=vision_tower_cfg, **kwargs)
-        else:
-            return CLIPVisionTower(vision_tower_name, args=vision_tower_cfg, **kwargs)
-
-    raise ValueError(f"Unknown vision tower: {vision_tower_type}")
+    return DINOVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
--- mllm/llava/model/multimodal_projector/builder.py
+++ mllm/llava/model/multimodal_projector/builder.py
@@ -3,23 +3,8 @@ import torch.nn as nn
 from .attentional_pooler import AttentionalPoolProjector
 
 
-class IdentityMap(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x, *args, **kwargs):
-        return x
-
-    @property
-    def config(self):
-        return {"mm_projector_type": "identity"}
-
-
 def build_vision_projector(config, delay_load=False, **kwargs):
     projector_type = getattr(config, "mm_projector_type", "linear")
-
-    if projector_type == "linear":
-        return nn.Linear(config.mm_hidden_size, config.hidden_size)
 
     if projector_type == "attn_pool":
         mm_projector = AttentionalPoolProjector(
@@ -27,8 +12,5 @@ def build_vision_projector(config, delay_load=False, **kwargs):
             context_dim=config.mm_context_size,
         )
         return mm_projector
-
-    if projector_type == "identity":
-        return IdentityMap()
 
     raise ValueError(f"Unknown projector type: {projector_type}")
