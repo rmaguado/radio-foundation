28eefbc931119dd02fd97c0fdc41c75ab9d74881
rmaguado <ruben.morenoaguadog@gmail.com>
2025-03-28 03:18:08 +0100

    redesign conversation templates for llama3

--- mllm/llava/conversation.py
+++ mllm/llava/conversation.py
@@ -2,12 +2,14 @@ import dataclasses
 from enum import auto, Enum
 from typing import List
 
+from mllm.llava.constants import DEFAULT_IMAGE_TOKEN
+
 
 class SeparatorStyle(Enum):
     """Different separator style."""
 
     PLAIN = auto()
-    TWO = auto()
+    LLAMA_3 = auto()
 
 
 @dataclasses.dataclass
@@ -18,92 +20,58 @@ class Conversation:
     roles: List[str]
     messages: List[List[str]]
     offset: int
-    sep_style: SeparatorStyle = SeparatorStyle.PLAIN
-    sep: str = "\n"
-    sep2: str = None
+    sep_style: SeparatorStyle
     multimodal: bool = False
+
+    def parse_plain(self, messages):
+        prompt = self.system
+        if self.multimodal:
+            prompt += f"<Image>{DEFAULT_IMAGE_TOKEN}</Image>"
+        for i, (role, message) in enumerate(messages):
+            if message:
+                prompt += "###" + message + "\n"
+        return prompt
+
+    def parse_llama_3(self, messagse):
+        prompt = f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>{self.system}<|eot_id|>"
+        if self.multimodal:
+            prompt += f"<Image>{DEFAULT_IMAGE_TOKEN}</Image>"
+        for i, (role, message) in enumerate(messages):
+            prompt += f"<|start_header_id|>{role}<|end_header_id|>"
+            if message:
+                prompt += f"{message}<|eot_id|>"
+        return prompt
 
     def get_prompt(self):
         messages = self.messages
-        if len(messages) > 0 and type(messages[0][1]) is tuple:
-            messages = self.messages.copy()
-            init_role, init_msg = messages[0].copy()
-            init_msg = init_msg[0].replace("<image>", "").strip()
-            if self.multimodal:
-                messages[0] = (init_role, init_msg)
-                messages.insert(0, (self.roles[0], "<Image><image></Image>"))
-                messages.insert(1, (self.roles[1], "Received."))
-            else:
-                messages[0] = (init_role, "<image>\n" + init_msg)
 
         if self.sep_style == SeparatorStyle.PLAIN:
-            seps = [self.sep, self.sep2]
-            ret = self.system
-            for i, (role, message) in enumerate(messages):
-                if message:
-                    if type(message) is tuple:
-                        message, _, _ = message
-                    ret += message + seps[i % 2]
-                else:
-                    ret += ""
-        elif self.sep_style == SeparatorStyle.TWO:
-            seps = [self.sep, self.sep2]
-            ret = self.system + seps[0]
-            for i, (role, message) in enumerate(messages):
-                if message:
-                    if type(message) is tuple:
-                        message, _, _ = message
-                    ret += role + ": " + message + seps[i % 2]
-                else:
-                    ret += role + ":"
-        else:
-            raise ValueError(f"Invalid style: {self.sep_style}")
+            return self.parse_plain(messages)
 
-        return ret
+        elif self.sep_style == SeparatorStyle.LLAMA_3:
+            return self.parse_llama_3(messages)
+
+        raise ValueError(f"Invalid style: {self.sep_style}")
 
     def append_message(self, role, message):
         self.messages.append([role, message])
-
-    def get_images(self):
-        images = []
-        for i, (role, msg) in enumerate(self.messages[self.offset :]):
-            if i % 2 == 0:
-                if type(msg) is tuple:
-                    msg, image = msg
-                    images.append(image)
-        return images
 
     def copy(self):
         return Conversation(
             system=self.system,
             roles=self.roles,
-            messages=[[x, y] for x, y in self.messages],
+            messages=self.messages.copy(),
             offset=self.offset,
             sep_style=self.sep_style,
-            sep=self.sep,
-            sep2=self.sep2,
             multimodal=self.multimodal,
         )
 
     def dict(self):
-        if len(self.get_images()) > 0:
-            return {
-                "system": self.system,
-                "roles": self.roles,
-                "messages": [
-                    [x, y[0] if type(y) is tuple else y] for x, y in self.messages
-                ],
-                "offset": self.offset,
-                "sep": self.sep,
-                "sep2": self.sep2,
-            }
         return {
             "system": self.system,
             "roles": self.roles,
             "messages": self.messages,
             "offset": self.offset,
-            "sep": self.sep,
-            "sep2": self.sep2,
         }
 
 
@@ -113,29 +81,15 @@ conv_llava_plain = Conversation(
     messages=(),
     offset=0,
     sep_style=SeparatorStyle.PLAIN,
-    sep="\n",
 )
 
 conv_llama_3 = Conversation(
-    system="A chat between a curious user and an artificial intelligence assistant. "
-    "The assistant gives helpful, detailed, and polite answers to the user's questions.",
-    roles=("user", "assistant"),
-    messages=(),
-    offset=0,
-    sep_style=SeparatorStyle.TWO,
-    sep=" ",
-    sep2="<|end_of_text|>",
-)
-
-conv_llama_3_mm = Conversation(
     system="A chat between a curious user and an artificial intelligence assistant. The assistant is able to understand the visual content that the user provides, and assist the user with a variety of tasks using natural language. The visual content will be provided with the following format: <Image>visual content</Image>.",
     roles=("user", "assistant"),
     multimodal=True,
     messages=(),
     offset=0,
-    sep_style=SeparatorStyle.TWO,
-    sep=" ",
-    sep2="<|end_of_text|>",
+    sep_style=SeparatorStyle.LLAMA_3,
 )
 
 
@@ -143,7 +97,6 @@ default_conversation = conv_llava_plain
 conv_templates = {
     "plain": conv_llava_plain,
     "llama_3": conv_llama_3,
-    "llama_3_mm": conv_llama_3_mm,
 }
 
 
