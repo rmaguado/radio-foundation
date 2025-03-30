ccfb8d4826b9e9bc366fab766c283cecfa60d73d
rmaguado <ruben.morenoaguadog@gmail.com>
2025-03-30 03:33:11 +0200

    import logging

--- mllm/llava/train/train_xformers.py
+++ mllm/llava/train/train_xformers.py
@@ -1,3 +1,5 @@
+import logging
+
 from mllm.llava.train.llama_xformers_attn_monkey_patch import (
     replace_llama_attn_with_xformers_attn,
 )
@@ -5,6 +7,8 @@ from mllm.llava.train.llama_xformers_attn_monkey_patch import (
 replace_llama_attn_with_xformers_attn()
 
 from mllm.llava.train.train import train
+
+logger = logging.getLogger("mllm")
 
 if __name__ == "__main__":
     try:
