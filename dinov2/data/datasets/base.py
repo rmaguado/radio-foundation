5bec55116453c7d96dde2013a18c3e424ba9815b
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-03-30 15:45:35 +0200

    fix distributed import

--- dinov2/data/datasets/base.py
+++ dinov2/data/datasets/base.py
@@ -10,7 +10,7 @@ import os
 import sqlite3
 
 import torch.distributed as dist
-from distributed import is_main_process, is_enabled
+from dinov2.distributed import is_main_process, is_enabled
 
 
 class BaseDataset:
