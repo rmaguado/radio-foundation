c1ba037e2e4f3648727b9504d5ebb760fec6be6b
rmaguado <ruben.morenoaguadog@gmail.com>
2025-04-01 09:11:34 +0200

    add script to regenerate run file from template

---
+++ mllm/scripts/pretrain.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export DHOME="$HOME/projects/radio-foundation"
+export OUT="$DHOME/runs"
+
+# NAME NODE GPUS NWORKERS CONFIG
+
+export NAME=$1
+export NODE=$2
+export GPUS=$4
+export WORKERS=$5
+export CONFIG=$6
+
+if [ ! -z $5 ]; then 
+  printf "\nJob $NAME | $NODE:gpu:$GPUS | $WORKERS workers | config: $CONFIG \n\n"
+  mkdir -p $OUT/$NAME
+  envsubst '$NAME $NODE $GPUS $WORKERS $CONFIG $OUT' < $DHOME/mllm/scripts/pretrain.template > $OUT/$NAME/train.run
+  sbatch $OUT/$NAME/train.run
+
+else
+  printf "\nneed NAME NODE GPUS NWORKERS CONFIG\n\n"
+
+fi
