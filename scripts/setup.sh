1380fc91425c71698046915b4bc2f88562b18b2d
rmaguado <ruben.morenoaguadog@gmail.com>
2025-04-01 17:38:47 +0200

    remove ipykernel

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@ conda activate radio
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
 pip install -r requirements.txt
 pip install -U xformers --index-url https://download.pytorch.org/whl/cu126
-conda install -c conda-forge ipykernel
+pip install transformers 'accelerate>=0.26.0'
 conda install -c nvidia cuda-compiler
 pip install deepspeed
 pip install flash-attn --no-build-isolation
