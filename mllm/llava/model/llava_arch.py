fix control of toggle gradients, more efficient saving and loading, fix lora, +pruned some functions

mllm/llava/model/llava_arch.py
@@ -43,22 +43,27 @@ class LlavaMetaModel:
             vision_tower = vision_tower[0]
         return vision_tower
 
-    def initialize_vision_modules(self, model_args, device, torch_dtype, fsdp=None):
+    def get_mm_projector(self):
+        mm_projector = getattr(self, "mm_projector", None)
+        if type(mm_projector) is list:
+            mm_projector = mm_projector[0]
+        return mm_projector
+
+    def initialize_vision_modules(self, model_args, torch_dtype, fsdp=None):
         mm_vision_select_layer = model_args.mm_vision_select_layer
         mm_vision_select_feature = model_args.mm_vision_select_feature
-        mm_projector_checkpoint_path = model_args.mm_projector_checkpoint_path
+        checkpoint_path = model_args.checkpoint_path  # copy from here
 
         self.config.mm_vision_tower = model_args.vision_tower
         self.config.image_tokens = model_args.image_tokens
 
         vision_tower = build_vision_tower(model_args, torch_dtype=torch_dtype)
+        vision_tower.requires_grad_(False)
 
         if fsdp is not None and len(fsdp) > 0:
             self.vision_tower = [vision_tower]
         else:
             self.vision_tower = vision_tower
-
-        self.vision_tower.to(device=device)
 
         self.config.use_mm_proj = True
         self.config.mm_projector_type = getattr(
@@ -70,28 +75,6 @@ class LlavaMetaModel:
 
         self.mm_projector = build_vision_projector(self.config)
 
-        if mm_projector_checkpoint_path is not None:
-            mm_projector_weights = torch.load(
-                mm_projector_checkpoint_path, map_location="cpu"
-            )
-
-            def get_w(weights, keyword):
-                return {
-                    k.split(keyword + ".")[1]: v
-                    for k, v in weights.items()
-                    if keyword in k
-                }
-
-            self.mm_projector.load_state_dict(
-                get_w(mm_projector_weights, "mm_projector")
-            )
-
-            logger.info(
-                f"Loaded mm_projector from checkpoint: {mm_projector_checkpoint_path}"
-            )
-
-        self.mm_projector.to(device)
-
 
 class LlavaMetaForCausalLM(ABC):
 
@@ -101,6 +84,9 @@ class LlavaMetaForCausalLM(ABC):
 
     def get_vision_tower(self):
         return self.get_model().get_vision_tower()
+
+    def get_mm_projector(self):
+        return self.get_model().get_mm_projector()
 
     def encode_images(self, images):
         image_features = self.get_model().get_vision_tower()(images)

mllm/llava/train/lora.py
@@ -1,3 +1,8 @@
+"""
+Copied and modified from:
+https://github.com/2U1/Llama3.2-Vision-Finetune/blob/master/src/training/train.py#L73
+"""
+
 import torch
 import logging
 
@@ -5,32 +10,30 @@ import logging
 logger = logging.getLogger("DeepSpeed")
 
 
-def find_all_linear_names(model):
-    cls = torch.nn.Linear
-    lora_module_names = set()
-    multimodal_keywords = ["mm_projector", "vision_tower"]
-    for name, module in model.named_modules():
-        if any(mm_keyword in name for mm_keyword in multimodal_keywords): # TODO: don't skip these
-            continue
-        if isinstance(module, cls):
-            names = name.split(".")
-            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+def find_target_linear_names(model):
+    lora_namespan_exclude = ["lm_head", "embed_tokens", "mm_projector"]
+    linear_cls = torch.nn.modules.Linear
+    embedding_cls = torch.nn.modules.Embedding
+    lora_module_names = []
 
-    if "lm_head" in lora_module_names:  # needed for 16-bit
-        lora_module_names.remove("lm_head")
-    return list(lora_module_names)
+    for name, module in model.named_modules():
+        if any(ex_keyword in name for ex_keyword in lora_namespan_exclude):
+            continue
+        if isinstance(module, (linear_cls, embedding_cls)):
+            lora_module_names.append(name)
+
+    return lora_module_names
 
 
 def configure_lora(model, training_args):
     from peft import LoraConfig, get_peft_model
 
-    lora_config = LoraConfig(
+    peft_config = LoraConfig(
         r=training_args.lora_r,
         lora_alpha=training_args.lora_alpha,
-        target_modules=find_all_linear_names(model),
+        target_modules=find_target_linear_names(model),
         lora_dropout=training_args.lora_dropout,
         bias=training_args.lora_bias,
-        task_type="CAUSAL_LM",
     )
     if training_args.bits == 16:
         if training_args.bf16:
@@ -38,4 +41,4 @@ def configure_lora(model, training_args):
         if training_args.fp16:
             model.to(torch.float16)
     logger.info("Adding LoRA adapters.")
-    return get_peft_model(model, lora_config)
+    return get_peft_model(model, peft_config)

mllm/llava/train/save.py
@@ -2,32 +2,10 @@ import torch
 import os
 import logging
 from transformers import Trainer
+import torch.distributed as dist
 
 
 logger = logging.getLogger("DeepSpeed")
-
-
-def save_model(training_args, model, trainer):
-    if training_args.lora_enable:
-        save_model_with_lora(training_args, model)
-    else:
-        safe_save_model_for_hf_trainer(
-            trainer=trainer, output_dir=training_args.output_dir
-        )
-
-
-def save_model_with_lora(training_args, model):
-    state_dict = get_peft_state_maybe_zero_3(
-        model.named_parameters(), training_args.lora_bias
-    )
-    non_lora_state_dict = get_peft_state_non_lora_maybe_zero_3(model.named_parameters())
-    if training_args.local_rank == 0 or training_args.local_rank == -1:
-        model.config.save_pretrained(training_args.output_dir)
-        model.save_pretrained(training_args.output_dir, state_dict=state_dict)
-        torch.save(
-            non_lora_state_dict,
-            os.path.join(training_args.output_dir, "non_lora_trainables.bin"),
-        )
 
 
 def maybe_zero_3(param, ignore_status=False, name=None):
@@ -83,55 +61,26 @@ def get_peft_state_non_lora_maybe_zero_3(named_params, require_grad_only=True):
     return to_return
 
 
-def get_mm_adapter_state_maybe_zero_3(named_params, keys_to_match):
-    to_return = {
-        k: t
-        for k, t in named_params
-        if any(key_match in k for key_match in keys_to_match)
-    }
-    to_return = {
-        k: maybe_zero_3(v, ignore_status=True).cpu() for k, v in to_return.items()
-    }
-    return to_return
+def save_model(training_args, model, output_dir):
 
+    state_dict = get_peft_state_non_lora_maybe_zero_3(
+        model.named_parameters(), require_grad_only=True
+    )
 
-def safe_save_model_for_hf_trainer(trainer: Trainer, output_dir: str):
-    """Collects the state dict and dump to disk."""
+    logger.info("gathered state_dict")
 
-    if not getattr(trainer.args, "freeze_projector", False):
-        # Only save Adapter
-        keys_to_match = ["mm_projector"]
-        if getattr(trainer.args, "use_im_start_end", False):
-            keys_to_match.extend(["embed_tokens", "embed_in"])
-
-        weight_to_save = get_mm_adapter_state_maybe_zero_3(
-            trainer.model.named_parameters(), keys_to_match
+    if training_args.lora_enable:
+        lora_state_dict = get_peft_state_maybe_zero_3(
+            model.named_parameters(), training_args.lora_bias
         )
-        trainer.model.config.save_pretrained(output_dir)
+        state_dict.update(lora_state_dict)
 
-        current_folder = output_dir.split("/")[-1]
-        parent_folder = os.path.dirname(output_dir)
-        if trainer.args.local_rank == 0 or trainer.args.local_rank == -1:
-            if current_folder.startswith("checkpoint-"):
-                mm_projector_folder = os.path.join(parent_folder, "mm_projector")
-                os.makedirs(mm_projector_folder, exist_ok=True)
-                torch.save(
-                    weight_to_save,
-                    os.path.join(mm_projector_folder, f"{current_folder}.bin"),
-                )
-            else:
-                torch.save(
-                    weight_to_save, os.path.join(output_dir, f"mm_projector.bin")
-                )
-        return
+    if training_args.local_rank < 1:
+        torch.save(
+            state_dict,
+            os.path.join(output_dir, "model.bin"),
+        )
 
-    if trainer.deepspeed:
-        torch.cuda.synchronize()
-        trainer.save_model(output_dir)
-        return
+    logger.info("waiting by barrier")
 
-    state_dict = trainer.model.state_dict()
-    if trainer.args.should_save:
-        cpu_state_dict = {key: value.cpu() for key, value in state_dict.items()}
-        del state_dict
-        trainer._save(output_dir, state_dict=cpu_state_dict)  # noqa
+    dist.barrier()

mllm/llava/train/train.py
@@ -18,6 +18,9 @@ import logging
 import pathlib
 import torch
 import transformers
+import os
+
+import torch.distributed as dist
 
 from mllm.llava.train.trainer import LLaVATrainer
 
@@ -39,6 +42,8 @@ def train(attn_implementation="flash_attention_2"):
 
     global local_rank
     local_rank = training_args.local_rank
+    if dist.is_initialized():
+        dist.barrier(device_ids=[local_rank])
 
     model = LlavaLlamaForCausalLM.from_pretrained(
         model_args.model_name_or_path,
@@ -48,9 +53,7 @@ def train(attn_implementation="flash_attention_2"):
     )
 
     model.config.use_cache = False
-
-    if model_args.freeze_backbone:
-        model.model.requires_grad_(False)
+    model.freeze_language()
 
     if training_args.gradient_checkpointing:
         if hasattr(model, "enable_input_require_grads"):
@@ -62,9 +65,6 @@ def train(attn_implementation="flash_attention_2"):
 
             model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
 
-    if training_args.lora_enable:
-        configure_lora(model, training_args)
-
     tokenizer = transformers.AutoTokenizer.from_pretrained(
         model_args.model_name_or_path,
         cache_dir=training_args.cache_dir,
@@ -75,20 +75,38 @@ def train(attn_implementation="flash_attention_2"):
 
     model.get_model().initialize_vision_modules(
         model_args=model_args,
-        device=training_args.device,
         torch_dtype=torch.bfloat16 if training_args.bf16 else torch.float16,
         fsdp=training_args.fsdp,
     )
 
+    if training_args.lora_enable:
+        configure_lora(model, training_args)
+
+    if model_args.checkpoint_path is not None:
+        pretrained_weights = torch.load(model_args.checkpoint_path, map_location="cpu")
+        model.load_state_dict(pretrained_weights, strict=False)
+
+        logger.info(f"Loaded weights from checkpoint: {model_args.checkpoint_path}")
+        logger.info(pretrained_weights.keys())
+
+    model.get_vision_tower().to(training_args.device)
+    model.get_mm_projector().to(training_args.device)
+
     model.config.tokenizer_model_max_length = tokenizer.model_max_length
 
-    model.config.freeze_projector = training_args.freeze_projector = (
-        model_args.freeze_projector
-    )
-    if not model_args.freeze_projector:
-        model.requires_grad_(False)  # CHECK THIS
-        for p in model.get_model().mm_projector.parameters():
-            p.requires_grad = True
+    for p in model.get_model().mm_projector.parameters():
+        p.requires_grad = True
+
+    run_dir = os.path.dirname(training_args.output_dir)
+    with open(
+        os.path.join(run_dir, "gradients.txt"),
+        mode="w",
+        encoding="utf-8",
+    ) as f:
+        for name, module in model.named_modules():
+            if name:
+                has_gradients = any(p.requires_grad for p in module.parameters())
+                f.write(f"{name} {has_gradients}\n")
 
     model.config.mm_projector_lr = training_args.mm_projector_lr
     model.initialize_vision_tokenizer(model_args, tokenizer=tokenizer)
@@ -106,7 +124,10 @@ def train(attn_implementation="flash_attention_2"):
 
     model.config.use_cache = True
 
-    save_model(training_args, model, trainer)
+    final_dir = os.path.join(run_dir, "final")
+    os.makedirs(final_dir, exist_ok=True)
+
+    save_model(training_args, model, final_dir)
 
 
 if __name__ == "__main__":

mllm/llava/train/trainer.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import logging
 
 from torch.utils.data import Sampler
 
@@ -12,33 +13,10 @@ from transformers.trainer import (
 )
 from typing import List, Optional
 
-
-def maybe_zero_3(param, ignore_status=False, name=None):
-    from deepspeed import zero
-    from deepspeed.runtime.zero.partition_parameters import ZeroParamStatus
-
-    if hasattr(param, "ds_id"):
-        if param.ds_status == ZeroParamStatus.NOT_AVAILABLE:
-            if not ignore_status:
-                print(name, "no ignore status")
-        with zero.GatheredParameters([param]):
-            param = param.data.detach().cpu().clone()
-    else:
-        param = param.detach().cpu().clone()
-    return param
+from mllm.llava.train.save import save_model
 
 
-def get_mm_adapter_state_maybe_zero_3(named_params, keys_to_match):
-    to_return = {
-        k: t
-        for k, t in named_params
-        if any(key_match in k for key_match in keys_to_match)
-    }
-    to_return = {
-        k: maybe_zero_3(v, ignore_status=True, name=k).cpu()
-        for k, v in to_return.items()
-    }
-    return to_return
+logger = logging.getLogger("DeepSpeed")
 
 
 def split_to_even_chunks(indices, lengths, num_chunks):
@@ -292,33 +270,24 @@ class LLaVATrainer(Trainer):
         return self.optimizer
 
     def _save_checkpoint(self, model, trial, metrics=None):
-        if not getattr(self.args, "freeze_projector", False):
-            from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
 
-            checkpoint_folder = f"{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}"
+        logger.info("called _save_checkpoint")
 
-            run_dir = self._get_output_dir(trial=trial)
-            output_dir = os.path.join(run_dir, checkpoint_folder)
+        from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
 
-            # Only save Adapter
-            keys_to_match = ["mm_projector"]
-            if getattr(self.args, "use_im_start_end", False):
-                keys_to_match.extend(["embed_tokens", "embed_in"])
+        checkpoint_folder = f"{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}"
 
-            weight_to_save = get_mm_adapter_state_maybe_zero_3(
-                self.model.named_parameters(), keys_to_match
-            )
+        run_dir = self._get_output_dir(trial=trial)
+        output_dir = os.path.join(run_dir, checkpoint_folder)
+        os.makedirs(output_dir, exist_ok=True)
 
-            if self.args.local_rank == 0 or self.args.local_rank == -1:
-                self.model.config.save_pretrained(output_dir)
-                torch.save(
-                    weight_to_save, os.path.join(output_dir, f"mm_projector.bin")
-                )
-        else:
-            super()._save_checkpoint(model, trial, metrics)
+        save_model(self.args, self.model, output_dir)
+
+        logger.info("exited save_model")
 
     def _save(self, output_dir: Optional[str] = None, state_dict=None):
-        if not getattr(self.args, "freeze_projector", False):
-            pass  # CHECK THIS
-        else:
-            super()._save(output_dir, state_dict)
+        logger.info("called _save")
+        # super()._save(output_dir, state_dict)
+        save_model(self.args, self.model, output_dir)
+
+        logger.info("exited save_model")