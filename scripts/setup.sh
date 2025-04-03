fd054194f33cf61f42a93b65db276718aa3aa789
rmaguado <ruben.morenoaguadog@gmail.com>
2025-04-03 23:58:46 +0200

    downgrade everything to cuda12.4 and switch order around

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,10 @@
 conda create -n radio python=3.10 -y
 conda activate radio
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 pip install -r requirements.txt
-pip install -U xformers --index-url https://download.pytorch.org/whl/cu126
+pip install -U xformers --index-url https://download.pytorch.org/whl/cu124
 pip install transformers 'accelerate>=0.26.0'
-conda install -c nvidia cuda-compiler
-pip install deepspeed
 pip install flash-attn --no-build-isolation
 pip install peft
+conda install nvidia/label/cuda-12.4.1::cuda-compiler
+pip install deepspeed
