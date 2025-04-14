08a675e23190bc6a6e8e4a014dc6af8994925f2b
rmaguado <ruben.morenoaguadog@gmail.com>
2025-04-14 14:57:53 +0200

    fix pass arguments into correct tempalte

--- scripts/evaluate/ct_rate/test.sh
+++ scripts/evaluate/ct_rate/test.sh
@@ -22,7 +22,7 @@ fi
 mkdir -p $OUT/$JOB_NAME
 
 envsubst '$JOB_NAME $RUN_NAME $CHECKPOINT_NAME $NODE $GPUS $WORKERS $OUT $LABELS $FEATURES $EPOCHS' \
-    < $PWD/scripts/evaluate/ct_rate/cv.template \
+    < $PWD/scripts/evaluate/ct_rate/test.template \
     > $OUT/$JOB_NAME/$JOB_NAME.run
 
 sbatch $OUT/$JOB_NAME/$JOB_NAME.run
