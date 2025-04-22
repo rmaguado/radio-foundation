29088ec2cdb213001457156fbd37cfd18290a249
rmaguado <ruben.morenoaguadog@gmail.com>
2025-04-22 13:13:26 +0200

    polish parse_claims pipeline for ground truth reports

--- mllm/evaluation/parse_claims.py
+++ mllm/evaluation/parse_claims.py
@@ -7,7 +7,7 @@ import concurrent.futures
 import time
 
 
-with open("prompts/parse_claims.txt", "r") as f:
+with open("mllm/evaluation/prompts/parse_claims.txt", "r") as f:
     PARSE_CLAIMS_PROMPT = f.read()
 
 
@@ -26,14 +26,26 @@ def parse_claims(report_id, report, output_file, client):
     """
     Parses the claims from the report using the specified system prompt.
     """
+    t0 = time.time()
     try:
         response = get_response(PARSE_CLAIMS_PROMPT, report, client)
 
+        start_tag, end_tag = "<output>", "</output>"
+        if response.count(start_tag) == 1 and response.count(end_tag) == 1:
+            structured_report = response.split(start_tag)[1].split(end_tag)[0].strip()
+        else:
+            structured_report = response
+            logging.warning(f"Formatting issue for report {report_id}.")
+
         with open(output_file, "w", encoding="utf-8") as f:
-            f.write(response)
+            f.write(structured_report)
+
+        tf = time.time() - t0
+        logging.info(f"Parsed report {report_id} in {tf:.02f} seconds.")
 
     except Exception as e:
         logging.error(f"Error processing report {report_id}: {e}")
+
     time.sleep(0.2)
 
 
@@ -56,7 +68,7 @@ def parse_ground_truths(hosts):
 
     processed_ids = set()
     for report_id in df["report_id"].unique():
-        if os.path.exists(os.path.join(output_path, report_id + ".csv")):
+        if os.path.exists(os.path.join(output_path, f"{report_id}.txt")):
             processed_ids.add(report_id)
 
     df = df[~df["report_id"].isin(processed_ids)].drop_duplicates(subset=["report_id"])
@@ -65,9 +77,9 @@ def parse_ground_truths(hosts):
         futures = []
         for i, (_, row) in enumerate(df.iterrows()):
             client = clients[i % len(hosts)]
-            report = row["Findings_EN"]
+            report = row["report"]
             report_id = row["report_id"]
-            output_file = os.path.join(output_path, report_id + ".csv")
+            output_file = os.path.join(output_path, f"{report_id}.txt")
             futures.append(
                 executor.submit(parse_claims, report_id, report, output_file, client)
             )
@@ -77,8 +89,9 @@ def parse_ground_truths(hosts):
 
 
 if __name__ == "__main__":
-    ports_cc2 = list(range(11434, 11434 + 8))
-    ports_cc3 = list(range(11434, 11434 + 4))
-    hosts_cc2 = [f"http://127.0.0.1:{port}" for port in ports_cc2]
-    hosts_cc3 = [f"http://192.168.36.203:{port}" for port in ports_cc3]
-    parse_ground_truths(hosts_cc2 + hosts_cc3)
+    # ports_cc2 = list(range(11434, 11434 + 8))
+    # ports_cc3 = list(range(11434, 11434 + 4))
+    # hosts_cc2 = [f"http://127.0.0.1:{port}" for port in ports_cc2]
+    # hosts_cc3 = [f"http://192.168.36.203:{port}" for port in ports_cc3]
+    hosts = ["http://127.0.0.1:11434"]
+    parse_ground_truths(hosts)
