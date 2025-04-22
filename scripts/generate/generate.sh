1bce9ff91ac89c79cd7c8585cc9fa3c912b53ce2
rmaguado <ruben.morenoaguadog@gmail.com>
2025-04-22 13:16:19 +0200

    define path to run and path to checkpoint separately

--- scripts/generate/generate.sh
+++ scripts/generate/generate.sh
@@ -8,19 +8,20 @@ export OUT="$DHOME/runs"
 export NAME=$1
 export NODE=$2
 export CONFIG=$3
-export CHECKPOINT=$4
+export PATH_TO_RUN=$4
+export CHECKPOINT=$5
 export GPUS=1
 export WORKERS=8
 
 export MEMORY=$(($GPUS*96))GB
 
-if [ ! -z $4 ]; then 
+if [ ! -z $5 ]; then 
   printf "\n     name | $NAME\n     node | $NODE\nresources | gpu:$GPUS workers:$WORKERS \n\n"
   mkdir -p $OUT/$NAME
-  envsubst '$NAME $NODE $GPUS $WORKERS $CONFIG $OUT $CHECKPOINT $MEMORY' < $DHOME/scripts/generate/generate.template > $OUT/$NAME/train.run
+  envsubst '$NAME $NODE $GPUS $WORKERS $CONFIG $OUT $PATH_TO_RUN $CHECKPOINT $MEMORY' < $DHOME/scripts/generate/generate.template > $OUT/$NAME/train.run
   sbatch $OUT/$NAME/train.run
 
 else
-  printf "\nneed NAME NODE CONFIG CHECKPOINT\n\n"
+  printf "\nneed NAME NODE CONFIG PATH_TO_RUN CHECKPOINT\n\n"
 
 fi
