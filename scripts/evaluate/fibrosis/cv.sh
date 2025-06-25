068a6f7f8cea31303e199b8e84de1ebb540c3ba9
rmaguado <ruben.morenoaguadog@gmail.com>
2025-06-25 20:25:40 +0200

    training scripts for covid and fibrosis

---
+++ scripts/evaluate/fibrosis/cv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export DHOME="$HOME/projects/radio-foundation"
+export OUT="$DHOME/runs"
+
+export JOB_NAME=$1
+export RUN_NAME=$2
+export CHECKPOINT_NAME=$3
+export NODE=$4
+export GPUS=$5
+export WORKERS=$6
+export FEATURES=$7
+export EPOCHS=$8
+export EMBED_DIM=$9
+
+if [ "$#" -ne 9 ]; then
+    echo "Usage: $0 JOB_NAME RUN_NAME CHECKPOINT_NAME NODE GPUS WORKERS FEATURES EPOCHS EMBED_DIM"
+    exit 1
+fi
+
+
+mkdir -p $OUT/$JOB_NAME
+
+envsubst '$JOB_NAME $RUN_NAME $CHECKPOINT_NAME $NODE $GPUS $WORKERS $OUT $FEATURES $EPOCHS $EMBED_DIM' \
+    < $PWD/scripts/evaluate/fibrosis/cv.template \
+    > $OUT/$JOB_NAME/$JOB_NAME.run
+
+sbatch $OUT/$JOB_NAME/$JOB_NAME.run
