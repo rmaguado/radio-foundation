852090c104275af001d12ab14f9d7a6f2d36ae37
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-06-27 16:47:14 +0100

    fix spacing choice for isotropic view

--- dinov2/data/datasets.py
+++ dinov2/data/datasets.py
@@ -1,5 +1,6 @@
 import os
 import torch
+from typing import List, Tuple, Any
 import polars as pl
 import numpy as np
 import SimpleITK as sitk
@@ -43,33 +44,33 @@ class VolumeDataset:
     def __len__(self) -> int:
         return len(self.df)
 
-    def get_image_data(idx):
+    def get_image_data(self, idx: int) -> sitk.Image:
         raise NotImplementedError
 
-    def __getitem__(self, idx: int):
-        image = self.get_image_data(index)
+    def __getitem__(self, idx: int) -> torch.Tensor:
+        image = self.get_image_data(idx)
 
         original_spacing = image.GetSpacing()
         ratio = max(original_spacing) / min(original_spacing)
 
-        if ratio < 3.0:
-            if ratio > 1.1:
-                image = self.resample_to_isotropic(image)
-            is_isotropic = True
+        if ratio > 3.0:
+            new_spacing = (max(original_spacing) / 2,) * 3
         else:
-            is_isotropic = False
+            new_spacing = (min(original_spacing),) * 3
 
-        orientation = image.GetDirection()
+        image = self.resample_to_isotropic(image, new_spacing)
+
+        # orientation = image.GetDirection()
 
         volume = sitk.GetArrayFromImage(image)  # (slices, height, width)
         volume_tensor = torch.tensor(volume, dtype=torch.float32)
 
-        return volume_tensor, is_isotropic, orientation
+        return volume_tensor
 
 
 class DicomVolumeDataset(VolumeDataset):
 
-    def get_image_data(self, idx: int) -> tuple[torch.Tensor, bool, tuple]:
+    def get_image_data(self, idx: int) -> sitk.Image:
         meta = self.df.row(idx)
         dicom_folder = os.path.join(self.root_path, meta["path"])
 
@@ -85,9 +86,9 @@ class DicomVolumeDataset(VolumeDataset):
 
 class NiftiVolumeDataset(VolumeDataset):
 
-    def get_image_data(self, idx: int) -> tuple[torch.Tensor, bool, tuple]:
+    def get_image_data(self, idx: int) -> sitk.Image:
         meta = self.df.row(idx)
-        dicom_folder = os.path.join(self.root_path, meta["path"])
+        nifti_file_path = os.path.join(self.root_path, meta["path"])
 
         image = sitk.ReadImage(nifti_file_path)
 
@@ -115,8 +116,8 @@ class MultiDataset:
     def get_dataset_names(self) -> List[str]:
         return [d.dataset_name for d in self.datasets]
 
-    def _find_dataset_idx(self, index: int) -> int:
-        return np.searchsorted(self.cumulative_sizes, index, side="right")
+    def _find_dataset_idx(self, idx: int) -> int:
+        return int(np.searchsorted(self.cumulative_sizes, idx, side="right"))
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, Any]:
         dataset_idx = self._find_dataset_idx(index)
