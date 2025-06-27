b9de65917422394e933c7999b5c91b60255cb895
rmaguado <ruben.morenoaguadog@gmail.com>
2025-06-27 18:49:06 +0200

    remove old dataprep code

--- data/dataprep/__init__.py
+++
@@ -1,4 +0,0 @@
-from .CT.ct_database import CtDatabase
-from .CT.dicoms import DicomProcessor, DicomDatabase
-from .CT.niftis import NiftiProcessor, NiftiDatabase
-from .CT.npz import NpzProcessor, NpzDatabase
