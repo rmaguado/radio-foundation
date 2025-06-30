68180853b804a04361b009427d03b4a663aa8621
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-06-30 11:23:48 +0100

    remove sk centering

--- dinov2/loss/ibot_patch_loss.py
+++ dinov2/loss/ibot_patch_loss.py
@@ -15,9 +15,9 @@ logger = logging.getLogger("dinov2")
 
 
 try:
-    from xformers.ops import cross_entropy  # type: ignore
+    from xformers.ops import cross_entropy
 
-    def lossfunc(t, s, temp):  # type: ignore
+    def lossfunc(t, s, temp):
         s = s.float()
         t = t.float()
         if s.ndim == 2:
@@ -47,53 +47,9 @@ class iBOTPatchLoss(nn.Module):
     @torch.no_grad()
     def softmax_center_teacher(self, teacher_patch_tokens, teacher_temp):
         self.apply_center_update()
-        # teacher centering and sharpening
-        #
-        # WARNING:
-        #   as self.center is a float32, everything gets casted to float32 afterwards
-        #
-        # teacher_patch_tokens = teacher_patch_tokens.float()
-        # return F.softmax((teacher_patch_tokens.sub_(self.center.to(teacher_patch_tokens.dtype))).mul_(1 / teacher_temp), dim=-1)
+        # WARNING: as self.center is a float32, everything gets casted to float32 afterwards
 
         return F.softmax((teacher_patch_tokens - self.center) / teacher_temp, dim=-1)
-
-        # this is experimental, keep everything in float16 and let's see what happens:
-        # return F.softmax((teacher_patch_tokens.sub_(self.center)) / teacher_temp, dim=-1)
-
-    @torch.no_grad()
-    def sinkhorn_knopp_teacher(
-        self, teacher_output, teacher_temp, n_masked_patches_tensor, n_iterations=3
-    ):
-        teacher_output = teacher_output.float()
-        # world_size = dist.get_world_size() if dist.is_initialized() else 1
-        Q = torch.exp(
-            teacher_output / teacher_temp
-        ).t()  # Q is K-by-B for consistency with notations from our paper
-        # B = Q.shape[1] * world_size # number of samples to assign
-        B = n_masked_patches_tensor
-        dist.all_reduce(B)
-        K = Q.shape[0]  # how many prototypes
-
-        # make the matrix sums to 1
-        sum_Q = torch.sum(Q)
-        if dist.is_initialized():
-            dist.all_reduce(sum_Q)
-        Q /= sum_Q
-
-        for it in range(n_iterations):
-            # normalize each row: total weight per prototype must be 1/K
-            sum_of_rows = torch.sum(Q, dim=1, keepdim=True)
-            if dist.is_initialized():
-                dist.all_reduce(sum_of_rows)
-            Q /= sum_of_rows
-            Q /= K
-
-            # normalize each column: total weight per sample must be 1/B
-            Q /= torch.sum(Q, dim=0, keepdim=True)
-            Q /= B
-
-        Q *= B  # the columns must sum to 1 so that Q is an assignment
-        return Q.t()
 
     def forward(self, student_patch_tokens, teacher_patch_tokens, student_masks_flat):
         """
@@ -120,7 +76,7 @@ class iBOTPatchLoss(nn.Module):
     ):
         t = teacher_patch_tokens_masked
         s = student_patch_tokens_masked
-        # loss = torch.sum(t * F.log_softmax(s / self.student_temp, dim=-1), dim=-1)
+
         loss = lossfunc(t, s, self.student_temp)
         if masks_weight is None:
             masks_weight = (
@@ -154,7 +110,11 @@ class iBOTPatchLoss(nn.Module):
 
             if self.reduce_handle is not None:
                 self.reduce_handle.wait()
-            _t = self.async_batch_center / (self.len_teacher_patch_tokens * world_size)  # type: ignore
+
+            assert self.async_batch_center is not None
+            assert self.len_teacher_patch_tokens is not None
+
+            _t = self.async_batch_center / (self.len_teacher_patch_tokens * world_size)
 
             self.center = self.center * self.center_momentum + _t * (
                 1 - self.center_momentum
