892c2e550b74295ccf71bfb030c5e4dd40c22645
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-07-04 21:53:26 +0100

    remove placeholder tests for now

--- dinov2/tests/test_configs.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from dinov2 import configs
-
-
-def test_validation_placeholder():
-    # TODO: Implement tests for configs/validation.py
-    assert True
--- dinov2/tests/test_logging.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from dinov2 import logging
-
-
-def test_helpers_placeholder():
-    # TODO: Implement tests for logging/helpers.py
-    assert True
