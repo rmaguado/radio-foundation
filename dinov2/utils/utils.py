6f6ee8e1ad8567d857849a595381137458f25b46
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-07-04 19:31:19 +0100

    remove unused function

--- dinov2/utils/utils.py
+++ dinov2/utils/utils.py
@@ -51,27 +51,6 @@ def fix_random_seeds(seed=31):
     random.seed(seed)
 
 
-def get_sha():
-    cwd = os.path.dirname(os.path.abspath(__file__))
-
-    def _run(command):
-        return subprocess.check_output(command, cwd=cwd).decode("ascii").strip()
-
-    sha = "N/A"
-    diff = "clean"
-    branch = "N/A"
-    try:
-        sha = _run(["git", "rev-parse", "HEAD"])
-        subprocess.check_output(["git", "diff"], cwd=cwd)
-        diff = _run(["git", "diff-index", "HEAD"])
-        diff = "has uncommitted changes" if diff else "clean"
-        branch = _run(["git", "rev-parse", "--abbrev-ref", "HEAD"])
-    except Exception:
-        pass
-    message = f"sha: {sha}, status: {diff}, branch: {branch}"
-    return message
-
-
 class CosineScheduler(object):
     def __init__(
         self,
@@ -103,14 +82,6 @@ class CosineScheduler(object):
             return self.final_value
         else:
             return self.schedule[it]
-
-
-def has_batchnorms(model):
-    bn_types = (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d, nn.SyncBatchNorm)
-    for name, module in model.named_modules():
-        if isinstance(module, bn_types):
-            return True
-    return False
 
 
 def custom_repr_nested(obj, indent=0):
