ecd16f234332fe1cd163eb0277c932a9251dfe2a
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-07-07 15:13:41 +0100

    don't use channel dim for 3D img

--- dinov2/tests/test_augmentations.py
+++ dinov2/tests/test_augmentations.py
@@ -6,7 +6,7 @@ from dinov2.configs import get_cfg_from_path
 
 @pytest.fixture
 def dummy_image3d():
-    return torch.rand(1, 512, 512, 512)
+    return torch.rand(512, 512, 512)
 
 @pytest.fixture
 def config():
@@ -19,10 +19,10 @@ def test_recursive_hierarchy(dummy_image3d, config):
 
     output = augmenter(dummy_image3d)
 
-    assert output["3d_global_ld"]["images"].shape == (1, 1, 112, 112, 112)
-    assert output["3d_local_ld"]["images"].shape == (1, 3, 1, 56, 56, 56)
-    assert output["2d_global_hd"]["images"].shape == (1, 3, 1, 1, 224, 224)
-    assert output["2d_local_hd"]["images"].shape == (1, 3, 2, 1, 1, 112, 112)
+    assert output["3d_global_ld"]["images"].shape == (1, 112, 112, 112)
+    assert output["3d_local_ld"]["images"].shape == (1, 3, 56, 56, 56)
+    assert output["2d_global_hd"]["images"].shape == (1, 3, 1, 224, 224)
+    assert output["2d_local_hd"]["images"].shape == (1, 3, 2, 1, 112, 112)
 
     assert output["3d_global_ld"]["view_shape"] == [1]
     assert output["3d_local_ld"]["view_shape"] == [1, 3]
