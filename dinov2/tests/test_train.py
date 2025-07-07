23c903616da1cadfde161ff099e3ef5aec507be1
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-07-07 20:08:28 +0100

    add e2d test for train

--- dinov2/tests/test_train.py
+++ dinov2/tests/test_train.py
@@ -1,86 +1,75 @@
 import pytest
 import torch
+from functools import partial
+
 from dinov2.configs import get_cfg_from_path
 from dinov2.train.ssl_meta_arch import SSLMetaArch
+from dinov2.data import collate_data_and_cast, MaskingGenerator, DataAugmentationDINO
 
 
 @pytest.fixture
-def arch():
-    cfg = get_cfg_from_path("dinov2/tests/configs/minimal_model.yaml")
-    return SSLMetaArch(cfg, device=torch.device("cpu"))
+def cfg():
+    return get_cfg_from_path("dinov2/tests/configs/minimal_model.yaml")
 
 
 @pytest.fixture
-def collated_views():
-    return {
-        "3d_global_ld": {
-            "images": torch.rand(8, 1, 112, 112, 112),
-            "is_target": True,
-            "targets": ["3d_global_ld"],
-            "embed_layer": "patch3d",
-            "mask_shape": (8, 8, 8),
-            "view_shape": (1,),
-            "masks": torch.rand(8, 1, 512) > 0.5,
-        },
-        "3d_local_ld": {
-            "images": torch.rand(8, 1, 3, 56, 56, 56),
-            "is_target": False,
-            "targets": ["3d_global_ld"],
-            "embed_layer": "patch3d",
-            "mask_shape": (4, 4, 4),
-            "view_shape": (1, 3),
-        },
-        "2d_global_hd": {
-            "images": torch.rand(8, 1, 3, 1, 224, 224),
-            "is_target": True,
-            "targets": ["3d_global_ld", "2d_global_hd"],
-            "embed_layer": "patch2d",
-            "mask_shape": (16, 16),
-            "view_shape": (1, 3),
-            "masks": torch.rand(8, 1, 3, 256) > 0.5,
-        },
-        "2d_local_hd": {
-            "images": torch.rand(8, 1, 3, 2, 1, 112, 112),
-            "is_target": False,
-            "targets": ["3d_global_ld", "2d_global_hd"],
-            "embed_layer": "patch2d",
-            "mask_shape": (8, 8),
-            "view_shape": (1, 3, 2),
-        },
-    }
+def arch(cfg):
+    return SSLMetaArch(cfg, device=torch.device("cpu"), dtype=torch.float32)
 
 
-def test_ssl_meta_arch_intermediate_steps(arch, collated_views):
+@pytest.fixture
+def collated_views(cfg):
+
+    dataset_config = cfg.datasets[0]
+
+    augmentation_module = DataAugmentationDINO(
+        config=cfg.augmentations.default_ct,
+        dataset_config=dataset_config,
+    )
+
+    mask_generator = MaskingGenerator()
+
+    collator = partial(
+        collate_data_and_cast,
+        mask_ratio_tuple=(0.1, 0.5),
+        mask_probability=0.5,
+        dtype=torch.float32,
+        mask_generator=mask_generator,
+    )
+
+    batch_size = 8
+    samples = [
+        augmentation_module(torch.rand(512, 512, 512)) for _ in range(batch_size)
+    ]
+
+    return collator(samples)
+
+
+def test_e2e(arch, collated_views):
+
+    total_loss, loss_dict = arch.train_model(collated_views, teacher_temp=1.0)
+
+
+def test_forward(arch, collated_views):
     teacher_temp = 1.0
 
-    # _prepare_inputs
     arch._prepare_inputs(collated_views)
 
-    # _run_teacher_pass
-    teacher_outputs = arch._run_teacher_pass(collated_views, teacher_temp)
+    with arch.autocast_ctx():
+        teacher_outputs = arch._run_teacher_pass(collated_views, teacher_temp)
+        student_outputs = arch._run_student_pass(collated_views)
 
-    # _run_student_pass
-    student_outputs = arch._run_student_pass(collated_views)
-
-    # _calculate_dino_loss
     dino_loss = arch._calculate_dino_loss(
         student_outputs["dino"], teacher_outputs["dino"], collated_views
     )
-    assert isinstance(dino_loss, torch.Tensor)
-
-    # _calculate_koleo_loss
+    ibot_loss = arch._calculate_ibot_loss(
+        student_outputs["ibot"],
+        teacher_outputs["ibot"],
+        student_outputs["mask_weights"],
+    )
     student_target_dino_tokens = {
         group_name: tokens
         for group_name, tokens in student_outputs["dino"].items()
         if collated_views[group_name]["is_target"]
     }
     koleo_loss = arch._calculate_koleo_loss(student_target_dino_tokens)
-    assert isinstance(koleo_loss, torch.Tensor)
-
-    # _calculate_ibot_loss
-    ibot_loss = arch._calculate_ibot_loss(
-        student_outputs["ibot"],
-        teacher_outputs["ibot"],
-        student_outputs["mask_weights"],
-    )
-    assert isinstance(ibot_loss, torch.Tensor)
