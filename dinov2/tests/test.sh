f47300af50cd30d4eb9c80bb143565529474d732
rmaguado <ruben.morenoaguadog@gmail.com>
2025-07-07 23:48:13 +0200

    add slurm script for tests

---
+++ dinov2/tests/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=dinov2tests
+#SBATCH --output=runs/dinov2tests/%j.out
+#SBATCH --nodes=1
+#SBATCH --nodelist=compute-cuda-01
+#SBATCH --cpus-per-task=1
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:RTX3090:1
+
+#SBATCH --mem=16GB
+#SBATCH --partition=odap-gpu
+#SBATCH --signal=USR2@120
+#SBATCH --time=14400
+
+source $HOME/.conda_rc
+conda activate radio
+cd $HOME/projects/radio-foundation
+export PYTHONPATH=$PWD
+
+pytest dinov2/tests/test_train.py --tb=long
