33b1587af0f217d70607d91c68834d870649f3f7
rmaguado <ruben.morenoaguadog@gmail.com>
2025-07-10 00:54:11 +0200

    use cc2

--- dinov2/tests/test.sh
+++ dinov2/tests/test.sh
@@ -3,13 +3,13 @@
 #SBATCH --job-name=dinov2tests
 #SBATCH --output=runs/dinov2tests/%j.out
 #SBATCH --nodes=1
-#SBATCH --nodelist=compute-cuda-01
+#SBATCH --nodelist=compute-cuda-02
 #SBATCH --cpus-per-task=1
 
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:RTX3090:1
+#SBATCH --gres=gpu:L40S:1
 
-#SBATCH --mem=16GB
+#SBATCH --mem=64GB
 #SBATCH --partition=odap-gpu
 #SBATCH --signal=USR2@120
 #SBATCH --time=14400
