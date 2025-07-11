44fcd7d13577f64f1ff92496a2b31b2e318ca15f
rmaguado <ruben.morenoaguadog@gmail.com>
2025-07-11 21:42:13 +0200

    fix dist function name

--- dinov2/train/utils.py
+++ dinov2/train/utils.py
@@ -43,11 +43,11 @@ def apply_gradient_operations(cfg, model, optimizer, accum_steps):
 
 
 def log_training_step(metric_logger, loss_dict, schedulers, iteration):
-    if distributed.get_global_size() > 1:
+    if distributed.get_world_size() > 1:
         for v in loss_dict.values():
             torch.distributed.all_reduce(v)
     loss_dict_reduced = {
-        k: v.item() / distributed.get_global_size() for k, v in loss_dict.items()
+        k: v.item() / distributed.get_world_size() for k, v in loss_dict.items()
     }
 
     if math.isnan(sum(loss_dict_reduced.values())):
