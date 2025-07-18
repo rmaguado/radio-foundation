daa33fa8ac588ce3dacc2f18510ee7e23c8f06f4
rmaguado <ruben.morenoaguadog@gmail.com>
2025-07-18 20:38:35 +0200

    fix unsupported bf16 for matplotlib

--- dinov2/tests/test_dataloader.py
+++ dinov2/tests/test_dataloader.py
@@ -16,43 +16,48 @@ logger.setLevel(logging.DEBUG)
 def cfg():
     return get_cfg_from_path("dinov2/configs/tests/minimal.yaml")
 
+
 @pytest.fixture
 def dataloader(cfg):
     inputs_dtype = torch.bfloat16
     return setup_dataloader(cfg, inputs_dtype)
 
+
 def test_dataloader_speed(cfg, dataloader):
     num_workers = cfg.train.num_workers
     logger.info(f"Using {num_workers} workers.")
     dataloader_iter = iter(dataloader)
-    
+
     for idx in range(32):
         t0 = time.time()
         data = next(dataloader_iter)
         tf = time.time() - t0
         logger.info(f"Batch {idx}: waited {tf:.06f} seconds.")
 
-    assert all(x in data.keys() for x in ["global_3d", "local_3d", "global_2d", "local_2d"])
+    assert all(
+        x in data.keys() for x in ["global_3d", "local_3d", "global_2d", "local_2d"]
+    )
+
 
 def test_dataloader_output(dataloader):
     dataloader_iter = iter(dataloader)
     data = next(dataloader_iter)
     output_path = "dinov2/tests/out"
 
-    global_3d = data["global_3d"]["images"]
+    global_3d = data["global_3d"]["images"].float().numpy()
     _, V, D, W, H = global_3d.shape
 
     plt.figure()
-    plt.imshow(global_3d[0,0,D//2,:,:], cmap="gray")
+    plt.imshow(global_3d[0, 0, D // 2, :, :], cmap="gray")
     plt.colorbar()
     plt.savefig(os.path.join(output_path, "global_3d_viewA.png"))
 
     plt.figure()
-    plt.imshow(global_3d[0,0,:,W//2,:], cmap="gray")
+    plt.imshow(global_3d[0, 0, :, W // 2, :], cmap="gray")
     plt.colorbar()
     plt.savefig(os.path.join(output_path, "global_3d_viewB.png"))
 
     plt.figure()
-    plt.imshow(global_3d[0,0,:,:,H//2], cmap="gray")
+    plt.imshow(global_3d[0, 0, :, :, H // 2], cmap="gray")
     plt.colorbar()
     plt.savefig(os.path.join(output_path, "global_3d_viewC.png"))
