23d6fca49d72d2e6191711f894977e3e0398a4e6
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-07-18 19:20:30 +0100

    reuse name argument for test file

--- scripts/tests/sjob.sh
+++ scripts/tests/sjob.sh
@@ -3,18 +3,17 @@
 export DHOME="$HOME/projects/radio-foundation"
 
 export NAME=$1
-export TESTFILE=$2
-export NODE=$3
-export GPUS=$4
-export GPUTYPE=$5
-export WORKERS=$6
-export MEM=$7
+export NODE=$2
+export GPUS=$3
+export GPUTYPE=$4
+export WORKERS=$5
+export MEM=$6
 
 export OUT="$DHOME/runs/$NAME"
 
 
 mkdir -p $OUT
 
-envsubst '$DHOME $OUT $NAME $TESTFILE $NODE $GPUS $GPUTYPE $WORKERS  $MEM' < $DHOME/scripts/tests/sjob.template > $OUT/test.run
+envsubst '$DHOME $OUT $NAME $NODE $GPUS $GPUTYPE $WORKERS  $MEM' < $DHOME/scripts/tests/sjob.template > $OUT/test.run
 
 sbatch $OUT/test.run
