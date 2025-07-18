80ef351f0303056ba816692a8ce8c9054d9f90d3
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2025-07-18 19:47:47 +0100

    add tests for forward function

--- dinov2/tests/test_train.py
+++ dinov2/tests/test_train.py
@@ -1,8 +1,10 @@
 import pytest
 import torch
+import time
 import logging
 
 from dinov2.configs import get_cfg_from_path
+from dinov2.train.setup import setup_dataloader
 from dinov2.train.ssl_meta_arch import SSLMetaArch
 
 logger = logging.getLogger("test")
@@ -20,7 +22,37 @@ def device():
 
 
 @pytest.fixture
-def arch(cfg, device):
-    arch = SSLMetaArch(cfg)
-    arch.to(device)
-    return arch
+def inputs_dtype():
+    return torch.bfloat16
+
+
+@pytest.fixture
+def dataloader(cfg, inputs_dtype):
+    return setup_dataloader(cfg, inputs_dtype)
+
+
+@pytest.fixture
+def model(cfg, device):
+    model = SSLMetaArch(cfg)
+    model.to(device)
+    return model
+
+
+def test_train_speed(model, dataloader, inputs_dtype):
+    dataloader_iter = iter(dataloader)
+
+    for idx in range(16):
+        data = next(dataloader_iter)
+
+        t0 = time.time()
+        with torch.autocast(device_type="cuda", enabled=True, dtype=inputs_dtype):
+            loss_accumulator, loss_dict = model.forward(data, teacher_temp=0.99)
+        tf = time.time() - t0
+
+        logger.info(f"Batch {idx}: forward took {tf:.06f} seconds.")
+
+        t0 = time.time()
+        loss_accumulator.backward()
+        tf = time.time() - t0
+
+        logger.info(f"Batch {idx}: backward took {tf:.06f} seconds.")
